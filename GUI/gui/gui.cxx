dd790c8610d0739e88e4136b1a7e5a092798fe7c
stipek <>
2004-07-02 15:48:05 +0000

    Reformat the unindented lines and play with some different startup scenarios.

--- GUI/gui/gui.cxx
+++ GUI/gui/gui.cxx
@@ -36,27 +36,27 @@ extern "C"
     qeventloop = new QEventLoop();
     qapplication = new QApplication( argc, argv );
 
-for(int i=0;i<argc;i++)
-{
-  if(strcmp(argv[i],"--no_splash") == 0 || 
-     strcmp(argv[i],"-ns") == 0 )
-  {
-    SPLASH = FALSE;
-  }
-}
+    for(int i=0;i<argc;i++)
+    {
+      if(strcmp(argv[i],"--no_splash") == 0 || 
+         strcmp(argv[i],"-ns") == 0 )
+      {
+        SPLASH = FALSE;
+      }
+    }
 
     QPixmap *splash_pixmap = NULL;
     QSplashScreen *splash = NULL;
-if( SPLASH )
-{
-    splash_pixmap = new QPixmap( splash_xpm );
-    splash = new QSplashScreen( *splash_pixmap );
-    splash->setCaption("splash");
-    splash->show();
-    splash->message( "Loading plugins" );
-    event_routine();
-    splash->raise();
-}
+    if( SPLASH )
+    {
+      splash_pixmap = new QPixmap( splash_xpm );
+      splash = new QSplashScreen( *splash_pixmap );
+      splash->setCaption("splash");
+      splash->show();
+      splash->message( "Loading plugins" );
+      event_routine();
+      splash->raise();
+    }
 
 #ifdef FONT_CHANGE
     QFont m_font = QFont("Helvetica", 9, QFont::Normal );
@@ -81,44 +81,68 @@ if( SPLASH )
     qapplication->connect( qapplication, SIGNAL( lastWindowClosed() ), qapplication, SLOT( quit() ) );
 //    printf("connect this up.\n");
 
+    if( SPLASH )
+    {
+      sleep(1);
+      splash->message( "OpenSpeedShop is brought to you by:");
+      splash->raise();
+      event_routine();
+      sleep(1);
+      splash->message( "Jim ... ");
+      splash->raise();
+      event_routine();
+      splash->message( "Jim ...  Steve ...");
+      splash->raise();
+      event_routine();
+      splash->message( "Jim ...  Steve ...  Jack ...  Amaury ...");
+      splash->raise();
+      event_routine();
+      sleep(1);
+      splash->message( "Al ...");
+      splash->raise();
+      event_routine();
+      splash->message( "Al ...  David ...");
+      splash->raise();
+      event_routine();
+      splash->message( "Al ...  David ...  Helen ...");
+      splash->raise();
+      event_routine();
+      sleep(1);
+      splash->message( "and Bill.");
+      splash->raise();
+      event_routine();
+      sleep(1);
+      splash->message( "... with financial help from the US Government....");
+      splash->raise();
+      event_routine();
+      sleep(1);
+      splash->finish(w);
+      delete splash;
+      delete splash_pixmap;
+    }
+
+printf("argc=%d\n", argc);
 if( SPLASH )
 {
-sleep(1);
-splash->message( "OpenSpeedShop is brought to you by:");
-splash->raise();
-event_routine();
-sleep(1);
-splash->message( "Jim ... ");
-splash->raise();
-event_routine();
-splash->message( "Jim ...  Steve ...");
-splash->raise();
-event_routine();
-splash->message( "Jim ...  Steve ...  Jack ...  Amaury ...");
-splash->raise();
-event_routine();
-sleep(1);
-splash->message( "Al ...");
-splash->raise();
-event_routine();
-splash->message( "Al ...  David ...");
-splash->raise();
-event_routine();
-splash->message( "Al ...  David ...  Helen ...");
-splash->raise();
-event_routine();
-sleep(1);
-splash->message( "and Bill.");
-splash->raise();
-event_routine();
-sleep(1);
-splash->message( "... with financial help from the US Government....");
-splash->raise();
-event_routine();
-sleep(1);
-splash->finish(w);
-    delete splash;
-    delete splash_pixmap;
+  if( argc == 1 )
+  { // Pretend absolutely no clues are there for us...
+printf("Pretend absolutely no clues are there for us...\n");
+    w->topPC->dl_create_and_add_panel("Intro Wizard", w->topPC);
+  } else if( argc == 2 )
+  {
+  // Pretend there is an a.out and/or experiment file in the directory.
+printf("Pretend there is an a.out and/or experiment file in the directory.\n");
+    w->topPC->dl_create_and_add_panel("Intro Wizard", w->topPC);
+    w->topPC->dl_create_and_add_panel("Getting Started", w->topPC);
+  } else if( argc == 3 )
+  { // Pretend there was an executable file given...
+printf("Pretend there was an executable file given...\n");
+    w->topPC->dl_create_and_add_panel("Getting Started", w->topPC);
+  } else if( argc == 4 )
+  {  // Pretend there was an experiment file given...
+printf("Pretend there was an experiment file given...\n");
+    w->topPC->dl_create_and_add_panel("pcSample Panel", w->topPC);
+  }
 }
     
     if( cliFLAG == 1 )
