417a180bccb36d032af6399fc8daf5de38ee5eab
stipek <>
2004-07-21 13:05:04 +0000

    Move the IntroWizardPanel to the Wizards subdirectory. GUI/Panels/IntroWizardPanel/local_plugin_info.hxx 1.3 Renamed to GUI/Panels/Wizards/IntroWizardPanel/local_plugin_info.hxx

---
+++ GUI/Panels/Wizards/IntroWizardPanel/local_plugin_info.hxx
@@ -0,0 +1,10 @@
+  plugin_entry->grouping = "";
+  plugin_entry->show_immediate = 0;
+  plugin_entry->plugin_description = "PLUGIN_DESCRIPTION";
+  plugin_entry->plugin_name =  "IntroWizardPanel.so";
+  plugin_entry->plugin_location = "$FUTURE_TOOL_PLUGIN_DIR";
+  plugin_entry->plugin_entry_point = "panel_init";
+  plugin_entry->menu_heading =  "&Wizards";
+  plugin_entry->menu_label =  "&Intro Wizard";
+  plugin_entry->menu_accel =  "Ctrl+I";
+  plugin_entry->panel_type =  "Intro Wizard";
