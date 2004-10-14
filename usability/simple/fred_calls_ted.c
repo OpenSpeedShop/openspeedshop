14fea337a9125c1674635436b25ab52d37810023
stipek <>
2004-10-14 17:15:58 +0000

    Update these two files with more current information.

--- usability/simple/fred_calls_ted.c
+++ usability/simple/fred_calls_ted.c
@@ -11,8 +11,4 @@ int main(int argc, char **argv)
   int i = 1;
 
   foo(&i);
-
-  printf("Set a breakpoint at this line!\n");
-
-  printf("The final value of small_array[9,9]=%d\n", small_array[9][9]);
 }
