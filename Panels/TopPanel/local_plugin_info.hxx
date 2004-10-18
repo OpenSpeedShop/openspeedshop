a1a2345468871febac082f83080f254c92b37f49
stipek <>
2004-10-18 13:29:26 +0000

    Make the TopPanel a hidden panel.

--- Panels/TopPanel/local_plugin_info.hxx
+++ Panels/TopPanel/local_plugin_info.hxx
@@ -4,7 +4,7 @@
   plugin_entry->plugin_name =  "TopPanel.so";
   plugin_entry->plugin_location = "$OPENSPEEDSHOP_PLUGIN_PATH";
   plugin_entry->plugin_entry_point = "panel_init";
-  plugin_entry->menu_heading =  "&Tools";
+  plugin_entry->menu_heading =  ""; // Leaving this blank hides the menu entry.
   plugin_entry->menu_label =  "&Top Panel";
   plugin_entry->menu_accel = "Ctrl+T";
   plugin_entry->panel_type =  "Top Panel";
