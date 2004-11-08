e4c55bb8e62d273f9ae5f7f014cb002571cfb8e4
stipek <>
2004-11-08 20:10:13 +0000

    Puts the gui in it's own thread.

--- gui/gui.cxx
+++ gui/gui.cxx
@@ -14,82 +14,30 @@ QEventLoop *qeventloop;
 #include "splash.xpm"
 #include <qsplashscreen.h>
 
+#include <pthread.h>
+#define PTMAX 10
+pthread_t phandle[PTMAX];
+#include "ArgClass.hxx"
+
+
 extern "C" 
 {
-  void usage()
+  // This routine starts another QApplication gui.  It is called from 
+  // gui_init to have the gui started in it's own thread.
+  void
+  guithreadinit(void *ptr)
   {
-    printf("openss, version - prepreprepreRelease 0.01\n");
-    printf("usage: openss [-f executable] [-h host] [-x experiment_name]\n");
-    printf("              [-x experiment_name]\n");
-    printf("              [-p process_ID [-r thread_rank] ]\n");
-    printf("              [-a \"command line args\"]\n\n");
-    printf("  -f : executable being measured\n");
-    printf("  -h : host to locate target executable or process\n");
-    printf("  -x : experiement name\n");
-printf("      o pssamp   - Actual CPU time at the source line, machine instruction,\n");
-printf("                   and function levels by sampling the program counter at\n");
-printf("                   10 or 1-millisecond intervals. See “PC Sampling\n");
-printf("                   Wizard.\n");
-printf("\n");
-printf("      o usertime - Inclusive and exclusive CPU time for each function by\n");
-printf("                   sampling the callstack at 30-millisecond intervals.\n");
-printf("                   See   User Time Wizard.\n");
-printf("\n");
-printf("      o fpe      - All floating-point exceptions, with the exception type\n");
-printf("                   and the call stack at the time of the exception. See\n");
-printf("                   FPE Tracing Wizard.\n");
-printf("\n");
-printf("      o hwc      - Counts at the source line, machine instruction, and\n");
-printf("                   function levels of various hardware events, including:\n");
-printf("                   clock cycles, graduated instructions, primary\n");
-printf("                   instruction cache misses, secondary instruction cache\n");
-printf("                   misses, primary data cache misses, secondary data\n");
-printf("                   cache misses, translation lookaside buffer (TLB)\n");
-printf("                   misses, and graduated floating-point instructions.\n");
-printf("                   See HW Counter Wizard.\n");
-printf("\n");
-printf("      o io       - Traces the following I/O system calls: read, readv,\n");
-printf("                   write, writev, open, close, dup, pipe, creat.\n");
-printf("                   See IO Wizard.\n");
-printf("\n");
-printf("      o mpi      - Traces calls to various MPI routines.\n");
-printf("                   See MPI Wizard.\n");
-    printf("  -p : pid of target process\n");
-    printf("  -r : rank of \n");
-    printf("  -a : quoted command line arguments to be passed to executable.\n");
-    printf("\n");
-    printf("Examples:\n");
-    printf("  To start Open/Speedshop simply type:\n");
-    printf("  $ openss\n");
-    printf("\n\n");
-    printf("  To load an executable into Open/Speedshop\n");
-    printf("  for eventual capturing of data:\n");
-    printf("  $ openss -f a.out\n");
-    printf("\n\n");
-    printf("  To attach to a running executable:\n");
-    printf("  $ openss -p 123456\n");
-    printf("\n\n");
-    printf("  To load an executable to run a simple timing (pcSampling) experiment:\n");
-    printf("  $ openss -f a.out -x pcsamp\n");
-  }
-  int
-  event_routine()
-  {
-//    printf("gui.so event_routine() entered\n");
-
-    // We need to only process for a while, then let the cli handle it's 
-    // events.
-    qeventloop->processEvents(QEventLoop::AllEvents, 2000);
-    return 1;
-  }
-
-  int
-  gui_init(int argc, char **argv, int cliFLAG)
-  {
+    ArgStruct *arg_struct = (ArgStruct *)ptr;
+    int argc = arg_struct->argc;
+    char **argv = arg_struct->argv;
     bool splashFLAG=TRUE;
-// printf("gui.so gui_init(cliFLAG=%d) entered\n", cliFLAG);
+    printf("A: gui.so gui_init() entered\n");
+    printf("argc=%d\n", argc);
+    for(int i=0;i<argc;i++)
+    {
+      printf("argv[%d]=%s\n", i, argv[i] );
+    }
   
-    qeventloop = new QEventLoop();
     qapplication = new QApplication( argc, argv );
 
     QString hostStr = QString::null;
@@ -129,8 +77,8 @@ printf("                   See MPI Wizard.\n");
         }
       } else if( arg == "-help" || arg == "--help" )
       {
-        usage();
-        return 0; // Failure to complete...
+//        usage();
+        return; // Failure to complete...
       } else if( arg == "-cli" )
       {
         // Valid: though not currently documented.   Just ignore 
@@ -138,14 +86,10 @@ printf("                   See MPI Wizard.\n");
       } else if( !arg.contains("openspeedshop") )
       {
         printf("Unknown argument syntax: argument in question: (%s)\n", arg.ascii() );
-        usage();
-        return 0; // Failure to complete...
+//        usage();
+        return; // Failure to complete...
       }
 
-    }
-    if( cliFLAG )
-    {
-      splashFLAG = FALSE;
     }
 
     QPixmap *splash_pixmap = NULL;
@@ -157,7 +101,6 @@ printf("                   See MPI Wizard.\n");
       splash->setCaption("splash");
       splash->show();
       splash->message( "Loading plugins" );
-      event_routine();
       splash->raise();
     }
 
@@ -171,21 +114,18 @@ printf("                   See MPI Wizard.\n");
     m_font.setFixedPitch(TRUE);
     qapplication->setFont(m_font);
 #endif // FONT_CHANGE
-if( cliFLAG )
-{
   char *fontname = getenv("OPENSPEEDSHOP_FONTNAME");
   if( fontname )
   {
-// printf("we have a fontname=(%s)\n", fontname );
+printf("we have a fontname=(%s)\n", fontname );
     QFont m_font = QFont(fontname);
     m_font.setRawName(fontname);
     qapplication->setFont(m_font);
   }
-}
-//    printf("create QApplication(A)\n");
+    printf("create QApplication(A)\n");
 
     OpenSpeedshop *w;
-// fprintf(stderr, "gui_init() entered.  call new OpenSpeedshop()\n");
+fprintf(stderr, "gui_init() entered.  call new OpenSpeedshop()\n");
     w = new OpenSpeedshop();
     w->executableName = executableStr;
     w->pidStr = pidStr;
@@ -193,7 +133,7 @@ if( cliFLAG )
     w->expStr = expStr;
     w->hostStr = hostStr;
     w->argsStr = argsStr;
-//    printf("create OpenSpeedshop()\n");
+    printf("create OpenSpeedshop()\n");
 
     if( w->expStr != NULL )
     {
@@ -223,38 +163,41 @@ if( cliFLAG )
     }
 
     w->show();
-//    printf("show OpenSpeedshop()\n");
+    printf("show OpenSpeedshop()\n");
 
     qapplication->connect( qapplication, SIGNAL( lastWindowClosed() ), qapplication, SLOT( quit() ) );
-//    printf("connect this up.\n");
+    printf("connect this up.\n");
 
     if( splashFLAG )
     {
       splash->raise();
-      event_routine();
       sleep(1);
       splash->raise();
       splash->message( "Plugins loaded..." );
-      event_routine();
       sleep(1);
       splash->raise();
-      event_routine();
       sleep(1);
       splash->raise();
-      event_routine();
       splash->finish(w);
       delete splash;
       delete splash_pixmap;
-  }
-
-    if( cliFLAG == 1 )
-    { // If this was launch from cli mode...
-      // It calls the qt event loop from within the cli event loop
-    } else
-    {
-      qeventloop->exec();
     }
 
-    return 1;
+    qapplication->exec();
+
+    return;
   }
+
+  // This is simple the routine that is called to start the gui it's own 
+  // thread.   Called from Start.cxx, when the gui is requested at startup.
+  // Otherwise, it can be called from the command line at any time to 
+  // initialize the gui.
+  int
+  gui_init( void *arg_struct )
+  {
+      int stat = pthread_create(&phandle[0], 0, (void *(*)(void *))guithreadinit,arg_struct);
+
+      return 1;
+  }
+
 }
