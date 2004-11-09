01deed0436b9a9d68592231d0dda74343a005272
dew <>
2004-11-09 22:22:10 +0000

    Define entry point for loging

--- pcli/PY_Input.cxx
+++ pcli/PY_Input.cxx
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 char *SpeedShop_ReadLine (int is_more);
+void  SpeedShop_Trace_ON (char *tofile);
+void  SpeedShop_Trace_OFF(void);
 
 static PyObject *SS_ReadLine (PyObject *self, PyObject *args) {
   int more = 0;
@@ -15,9 +17,21 @@ static PyObject *SS_ReadLine (PyObject *self, PyObject *args) {
   return Py_BuildValue("s", news);
 }
 
+static PyObject *SS_Record (PyObject *self, PyObject *args) {
+  char *tofile = NULL;
+  if (!PyArg_ParseTuple(args, "s", &tofile)) {
+    (void)SpeedShop_Trace_OFF ();
+  } else {
+    (void)SpeedShop_Trace_ON(tofile);
+  }
+  return Py_BuildValue("s", NULL);
+}
+
 static PyMethodDef PY_Input_Methods[] = {
     {"ReadLine",  SS_ReadLine, METH_VARARGS,
-     "Read a SpeedShop command."},
+       "Read a SpeedShop command."},
+    {"record",  SS_Record, METH_VARARGS,
+       "Control Command tracing."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
