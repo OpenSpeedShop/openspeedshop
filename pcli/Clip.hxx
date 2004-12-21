538f75dd0a91f133862a720d6f07b56db7aec790
dew <>
2004-12-21 22:32:39 +0000

    Modify the InputLineObject so that it can point to a_command_struct.
    The intent is that this will become the primary method for tracking
    user commands.  This object needs to be allocated as early as possible
    and needs to contain the current status of the command.  Later processing
    can define the COmmand Object and associated methods.

--- pcli/Clip.hxx
+++ pcli/Clip.hxx
@@ -8,25 +8,38 @@ enum Missing_oss_cmd_enum
                CMD_LOCATOR,
              };
 
+enum Input_Line_Status
+	{
+	  ILO_UNKNOWN,
+	  ILO_QUEUED_INPUT,
+	  ILO_IN_PARSER,
+	  ILO_EXECUTING,
+	  ILO_COMPLETE,
+	  ILO_ERROR
+	};
+
 
 // InputLineObject
 class InputLineObject
 {
  private:
+  Input_Line_Status status;	// Current state
   CMDID seq_num;		// Order this command was processed.
   time_t cmd_time;		// The time this command was sent.
   CMDWID who;			// Who constructed the command.  (i.e. "gui:PanelID" or "cli")
   bool waitForReturn;		// Always true for now...
-  oss_cmd_enum cmd_type;		// What type of information is being communicated.
+  oss_cmd_enum cmd_type;	// What type of information is being communicated.
 
   std::string command;		// The actual command to be executed.
   std::string msg_string;	// Intermediate processing information.
   CodeObjectLocator *COL;	// For CMD_LOCATOR types
   int64_t num_results;		// If result field is used, how big is it?
   void *result;			// Pointer to unlimited buffer space.
+  a_command_struct *Cmd_Obj;	// Pointer to associated Command Object
 
   void Basic_Initialization ()
     { 
+      status = ILO_UNKNOWN;
       seq_num = 0;
       cmd_time = time(0);
       who = 0;
@@ -37,6 +50,7 @@ class InputLineObject
       COL = NULL;
       num_results = 0;
       result = NULL;
+      Cmd_Obj = NULL;
     }
 
  public:
@@ -68,11 +82,27 @@ class InputLineObject
     {
     }
 
+  Input_Line_Status Status () {return status;}
+  void SetStatus (Input_Line_Status st) {status = st; cmd_time = time(0);}
   void SetSeq (CMDID seq) {seq_num = seq;}
   CMDID Seq () { return seq_num; }
   CMDWID Who () { return who;}
+  Input_Line_Status What() {return status;}
   time_t When () { return cmd_time; }
+  CMDID Where () { return seq_num; }
   oss_cmd_enum Action () { return cmd_type; }
   std::string Command () {return command;}
+
+  void Print (FILE *TFile) {
+      CMDID seq_num = Where();
+      CMDWID who = Who();
+      time_t cmd_time = When();
+      std::string command = Command();
+      fprintf(TFile,"C %lld (W%lld@%.24s): ",
+                    seq_num,who,ctime(&cmd_time));
+      if (command.length() != 0) {
+        fprintf(TFile,"%s\n", command.c_str());
+      }
+  }
 
 };
