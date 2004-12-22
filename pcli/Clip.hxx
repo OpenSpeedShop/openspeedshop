c4558952fda26c506974e76e9f175d0d64cc862e
dew <>
2004-12-22 21:29:53 +0000

    Clean up some of the declarations.

--- pcli/Clip.hxx
+++ pcli/Clip.hxx
@@ -1,41 +1,37 @@
-/* There would seem to be a need to extend oss_cmd_enum with the following: */
-enum Missing_oss_cmd_enum
-             { CMD_NONE,
-               CMD_COMPLETE,
-               CMD_ERROR,
-               CMD_PARSE,
+// Define the basic input line structure
+//
+// The intent is that this will become the primary method for tracking
+// user commands.  This object needs to be allocated as early as possible
+// and needs to contain the current status of the command.  Later processing
+// can define the COmmand object and associated methods.
 
-               CMD_LOCATOR,
-             };
 
 enum Input_Line_Status
-	{
-	  ILO_UNKNOWN,
-	  ILO_QUEUED_INPUT,
-	  ILO_IN_PARSER,
-	  ILO_EXECUTING,
-	  ILO_COMPLETE,
-	  ILO_ERROR
-	};
+{
+  ILO_UNKNOWN,
+  ILO_QUEUED_INPUT,
+  ILO_IN_PARSER,
+  ILO_EXECUTING,
+  ILO_COMPLETE,
+  ILO_ERROR
+};
 
 
 // InputLineObject
 class InputLineObject
 {
  private:
-  Input_Line_Status status;	// Current state
-  CMDID seq_num;		// Order this command was processed.
-  time_t cmd_time;		// The time this command was sent.
-  CMDWID who;			// Who constructed the command.  (i.e. "gui:PanelID" or "cli")
-  bool waitForReturn;		// Always true for now...
-  oss_cmd_enum cmd_type;	// What type of information is being communicated.
+  Input_Line_Status status;	// What is the current state.
+  CMDID seq_num;		// Where in sequence that this command was parsed.
+  time_t cmd_time;		// When the status field was set.
+  CMDWID who;			// Who constructed the command.  (i.e. "gui", "cli", or "?")
 
   std::string command;		// The actual command to be executed.
-  std::string msg_string;	// Intermediate processing information.
-  CodeObjectLocator *COL;	// For CMD_LOCATOR types
-  int64_t num_results;		// If result field is used, how big is it?
-  void *result;			// Pointer to unlimited buffer space.
-  a_command_struct *Cmd_Obj;	// Pointer to associated Command Object
+
+ // The following fields contain "result" information.
+  std::string msg_string;	// Intermediate processing or error information.
+  oss_cmd_enum cmd_type;	// Filled in when the parser determines the type of command.
+  a_command_struct *Cmd_Obj;	// Pointer to associated Command Object when constructed.
 
   void Basic_Initialization ()
     { 
@@ -43,13 +39,9 @@ class InputLineObject
       seq_num = 0;
       cmd_time = time(0);
       who = 0;
-      waitForReturn = true;
-      // cmd_type = CMD_NONE;
       command = std::string("");
       msg_string = std::string("");
-      COL = NULL;
-      num_results = 0;
-      result = NULL;
+      // cmd_type = CMD_NONE;
       Cmd_Obj = NULL;
     }
 
@@ -60,7 +52,6 @@ class InputLineObject
   InputLineObject (CMDWID From, std::string Cmd)
     { this->Basic_Initialization();
       who = From;
-      // cmd_type = CMD_PARSE,
       command = Cmd;
     }
   InputLineObject (oss_cmd_enum type)
@@ -82,12 +73,11 @@ class InputLineObject
     {
     }
 
-  Input_Line_Status Status () {return status;}
   void SetStatus (Input_Line_Status st) {status = st; cmd_time = time(0);}
   void SetSeq (CMDID seq) {seq_num = seq;}
   CMDID Seq () { return seq_num; }
   CMDWID Who () { return who;}
-  Input_Line_Status What() {return status;}
+  Input_Line_Status What() { return status; }
   time_t When () { return cmd_time; }
   CMDID Where () { return seq_num; }
   oss_cmd_enum Action () { return cmd_type; }
@@ -101,7 +91,11 @@ class InputLineObject
       fprintf(TFile,"C %lld (W%lld@%.24s): ",
                     seq_num,who,ctime(&cmd_time));
       if (command.length() != 0) {
-        fprintf(TFile,"%s\n", command.c_str());
+        fprintf(TFile,"%s", command.c_str());
+        int nline = strlen (command.c_str()) - 1;
+        if ((nline <= 0) || (command.c_str()[nline] != *("\n"))) {
+          fprintf(TFile,"\n");
+        }
       }
   }
 
