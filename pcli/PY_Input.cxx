e5f566a15458aeabc4f9fa4fa52ff79714274c0d
dew <>
2005-01-07 17:03:59 +0000

    Add routines to save and decode pointers to an InputLineObject so that Python can manipulate it and delay processing of commands.

--- pcli/PY_Input.cxx
+++ pcli/PY_Input.cxx
@@ -13,6 +13,10 @@ using namespace OpenSpeedShop::Framework;
 extern FILE *yyin;
 extern int yyparse (void);
 
+/* Global Data for tracking the current command line. */
+InputLineObject *Current_ILO = NULL;
+a_command_struct *Current_CO = NULL;
+
 static PyObject *SS_CallParser (PyObject *self, PyObject *args) {
     char *input_line = NULL;
     PyObject *p_object;
@@ -39,6 +43,70 @@ static PyObject *SS_CallParser (PyObject *self, PyObject *args) {
     return p_object;
 }
 
+// Save a pointer to the most recent Input_Line_Object.
+//
+// Since Python likes character strings better than anything,
+// Convert the pointer to a string so that Python can manipulate
+// it without knowing what it is.  The only use of this string
+// will be by the routine SS_ReadILO.
+//
+static PyObject *SS_DelayILO (PyObject *self, PyObject *args) {
+  char *seqnum = (char *)PyMem_Malloc(32);
+  snprintf( seqnum, 32, "%p", Current_ILO);
+  return Py_BuildValue("s", seqnum);
+}
+
+// Extract the input line from an Input_Line_Object.
+//
+// Save this line in a Python object.
+// While at it, set "current" pointers.
+//
+static PyObject *Prepare_Input_Line (InputLineObject *clip) {
+  int64_t buffer_size = clip->Command().length()+1;
+  bool need_newline = false;
+
+ // Python requires that every input line end with an EOL character.
+  if (clip->Command().c_str()[buffer_size-2] != *("\n")) {
+    buffer_size++;
+    need_newline = true;
+  }
+
+ // Use Python's memory allocator so that it will free the space when done.
+  char *sbuf = (char *)PyMem_Malloc(buffer_size);
+  strcpy (sbuf, clip->Command().c_str());
+  if (need_newline) {
+    sbuf[buffer_size-2] = *("\n");
+    sbuf[buffer_size-1] = *("\0");
+  }
+
+ // Set global variables to track the current line.
+  Current_ILO = clip;
+  Current_CO = NULL;
+
+  return Py_BuildValue("s", sbuf);
+}
+
+// Convert a Python character string to an ILO pointer.
+// Return the associated line of data to Python.
+//
+static PyObject *SS_ReadILO (PyObject *self, PyObject *args) {
+  char *ILO_Addr = NULL;
+  InputLineObject *clip = NULL;
+  
+  if (!PyArg_ParseTuple(args, "s", &ILO_Addr))
+        return NULL;
+  if (ILO_Addr == NULL) {
+   // This should be a more severe error
+    PyErr_SetString(PyExc_EOFError, "End Of File");
+    return NULL;
+  }
+  int r = sscanf (ILO_Addr, "%p", &clip);
+  return Prepare_Input_Line(clip);
+}
+
+// Get an Input_Line_Object from the input manager.
+// Return the associated line of data to Python.
+//
 static PyObject *SS_ReadLine (PyObject *self, PyObject *args) {
   int more = 0;
   
@@ -49,20 +117,7 @@ static PyObject *SS_ReadLine (PyObject *self, PyObject *args) {
     PyErr_SetString(PyExc_EOFError, "End Of File");
     return NULL;
   }
-  int64_t buffer_size = clip->Command().length()+1;
-  bool need_newline = false;
-  if (clip->Command().c_str()[buffer_size-2] != *("\n")) {
-    buffer_size++;
-    need_newline = true;
-  }
-  char *sbuf = (char *)PyMem_Malloc(buffer_size);
-  strcpy (sbuf, clip->Command().c_str());
-  if (need_newline) {
-    sbuf[buffer_size-2] = *("\n");
-    sbuf[buffer_size-1] = *("\0");
-  }
-  delete (clip);
-  return Py_BuildValue("s", sbuf);
+  return Prepare_Input_Line(clip);
 }
 
 static PyObject *SS_expFocus (PyObject *self, PyObject *args) {
@@ -92,7 +147,13 @@ static PyMethodDef PY_Input_Methods[] = {
 
     {"CallParser",  SS_CallParser, METH_VARARGS,
      "Call the YACC'd parser."},
-	
+
+    {"Save_ILO",  SS_DelayILO, METH_VARARGS,
+     "Save the Current_ILO."},
+
+    {"ReadILO",  SS_ReadILO, METH_VARARGS,
+     "Use a saved ILO to get a command."},
+
     {"ReadLine",  SS_ReadLine, METH_VARARGS,
      "Read a SpeedShop command."},
 
