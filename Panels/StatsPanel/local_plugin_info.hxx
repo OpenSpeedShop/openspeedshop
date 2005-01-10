af9bc073cecdf8b2ffede8617931a7bf32e42c4e
stipek <>
2005-01-10 23:12:00 +0000

    Missed a file with the last mod.

---
+++ Panels/StatsPanel/local_plugin_info.hxx
@@ -0,0 +1,10 @@
+  plugin_entry->grouping = "";
+  plugin_entry->show_immediate = 0;
+  plugin_entry->plugin_description = "PLUGIN_DESCRIPTION";
+  plugin_entry->plugin_name =  "StatsPanel.so";
+  plugin_entry->plugin_location = "$OPENSPEEDSHOP_PLUGIN_PATH";
+  plugin_entry->plugin_entry_point = "panel_init";
+  plugin_entry->menu_heading =  ""; // Leaving this blank hides the menu entry.
+  plugin_entry->menu_label =  "Stats Panel";
+  plugin_entry->menu_accel =  "Cntrl R";
+  plugin_entry->panel_type =  "Stats Panel";
