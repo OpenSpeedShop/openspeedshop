45d62d36f2e676b9aebe1a0fb704f9ccbe5ee631
amaury <>
2005-01-18 21:04:55 +0000

    Removed the standalone test. A few more things exported to Python

--- pcli/OSSParser/parser.cxx
+++ pcli/OSSParser/parser.cxx
@@ -6,14 +6,32 @@
 
 #include "pyinit.hxx"
 #include "parseresult.hxx"
+#include "Parser.hxx"
 #include "describe.hxx"
 
 using namespace OpenSpeedShop::cli;
 
+// This is the root of the parser
+// The parser takes a simple string as inoput and 
+// returns a tree C++ and Python can handle
+// There is no handling of any exception yet.
+
 ParseResult
 parseThisString( const std::string str )
 {
-    return ParseResult( "return", "aOK");
+    std::cout << "Parsing This string: " << str << std::endl;
+    // We really don't care about what's in the string for now.
+    if( str.find( "listPids" ))
+        return ParseResult( "listPids", ParseResult( "host", "localhost" ));
+    // Same Here, just create one
+    if( str.find( "expCreate" ))
+        return ParseResult( "expCreate", 
+                             ParseResult("file", "/home/foo/bin/bar" )
+                          +  ParseResult( "expType", "pcsamp" ));
+
+    // Problem: We just return an error
+    // XXXX: Exception here!
+    return ParseResult("Syntax", "Error" );
 }
 
 void
@@ -22,17 +40,6 @@ printParseResult( const ParseResult& p )
     std::cout << p.ptr() << std::endl;
 }
 
-#ifdef STANDALONE
-    
-main()
-{
-
-    ParseResult r = parseThisString("expAttach -p 1234 -t 4567 -h foobar");
-    printParseResult( r );
-    
-}
-
-#else
 
 // This is the Boost::Python export phase.
 // We just export the data type. 
@@ -52,7 +59,7 @@ BOOST_PYTHON_MODULE(OSSParser)
     //         +  str is any string to parse
     //         +  o is a ParseResult object that describes the parsing result
 
-    class_<OpenSpeedShop::cli::ParseResult>("ParseResult")
+    class_<ParseResult>("ParseResult")
 
         // The possible constructors
         .def( init< std::string, long>())
@@ -66,7 +73,8 @@ BOOST_PYTHON_MODULE(OSSParser)
         .def( "__getattribute__", &OpenSpeedShop::cli::ParseResult::getattr )
         ;
 
+    def("parseThisString", parseThisString );
 
-    def("parseThisString", &parseThisString );
+    class_<Parser>("Parser", init< std::string >())
+        ;
 }
-#endif
