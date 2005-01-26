f85a43daac4dc6f71085c417c645a42a4f2de772
dew <>
2005-01-26 18:44:46 +0000

    Create a ".hxx" that pulls in all the includes needed by the input manager.

---
+++ pcli/SS_Input_Manager.hxx
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>               /* for fstat() */
+#include <fcntl.h>
+#include <sys/mman.h>               /* for mmap() */
+#include <time.h>
+#include <stdio.h>
+#include <list>
+#include <inttypes.h>
+#include <stdexcept>
+#include <string>
+
+#ifndef PTHREAD_MUTEX_RECURSIVE_NP
+#define PTHREAD_MUTEX_RECURSIVE_NP 0
+#endif
+
+// for host name description
+     #include <sys/socket.h>
+     #include <netinet/in.h>
+     #include <netdb.h>
+
+#include "ToolAPI.hxx"
+using namespace OpenSpeedShop::Framework;
+
+#include "Commander.hxx"
+#include "support.h"
+#include "CommandObject.hxx"
+#include "Clip.hxx"
+#include "Experiment.hxx"
+
+#include "ArgClass.hxx"
