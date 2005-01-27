8014f06a217e96cba48a78bd1e2712e9c7dc173d
dew <>
2005-01-27 23:10:00 +0000

    Remove debug print statement that was left in last time around.

--- pcli/SS_Cmd_Execution.cxx
+++ pcli/SS_Cmd_Execution.cxx
@@ -2,6 +2,19 @@
 
 void SS_Execute_Cmd (CommandObject *cmd) {
   bool cmd_successful = false;
+  InputLineObject *clip = cmd->Clip();
+
+  switch (clip->What()) {
+  case ILO_IN_PARSER:
+    clip->SetStatus (ILO_EXECUTING);
+    break;
+  case ILO_EXECUTING:
+    break;
+  case ILO_COMPLETE:
+  case ILO_ERROR:
+    cmd->set_Status (CMD_ABORTED);
+    return;
+  }
 
   switch (cmd->Type()) {
   case CMD_EXP_ATTACH:
@@ -20,6 +33,10 @@ void SS_Execute_Cmd (CommandObject *cmd) {
     fprintf(stderr,"Command %d has not been implimented yet\n",cmd->Type());
   }
 
+  if (!cmd_successful) {
+    cmd->set_Status (CMD_ERROR);
+  }
  // For DEBUGING:
-  if (cmd_successful) cmd->Print(stdout);
+ // if (!cmd_successful) fprintf(stdout,"Command Error: ");
+ // cmd->Print(stdout);
 }
