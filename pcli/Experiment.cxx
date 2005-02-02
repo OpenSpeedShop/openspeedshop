a55fd71352cd082e8cb8780f461892b9f9d35982
dew <>
2005-02-02 22:38:45 +0000

    Add dummy routines for commands.

--- pcli/Experiment.cxx
+++ pcli/Experiment.cxx
@@ -7,6 +7,7 @@
 EXPID Experiment_Sequence_Number = 0;
 std::list<ExperimentObject *> ExperimentObject_list;
 
+// Experiment Building Block Commands
 
 ExperimentObject *Find_Experiment_Object (EXPID ExperimentID)
 {
@@ -69,8 +70,15 @@ bool SS_expAttach (CommandObject *cmd) {
   return true;
 }
 
-#define Define_Line_Info( a, b, c) (b = a->Clip(), \
-                                    c = ((b != NULL) ? b->Who() : 0))
+bool SS_expClose (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
 
 bool SS_expCreate (CommandObject *cmd) {
   InputLineObject *clip = cmd->Clip();
@@ -102,6 +110,36 @@ bool SS_expCreate (CommandObject *cmd) {
   return true;
 }
 
+bool SS_expDetach (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_expDisable (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_expEnable (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
 bool SS_expFocus  (CommandObject *cmd) {
   InputLineObject *clip = cmd->Clip();
   CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
@@ -121,6 +159,243 @@ bool SS_expFocus  (CommandObject *cmd) {
   cmd->set_Status(CMD_COMPLETE);
 
   return true;
+} 
+
+bool SS_expGo (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_expPause (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_expRestore (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_expSave (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_expSetParam (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_expView (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+// Information Commands
+
+bool SS_ListBreaks (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_ListExp (CommandObject *cmd) {
+  std::list<ExperimentObject *>::reverse_iterator expi;
+  for (expi = ExperimentObject_list.rbegin(); expi != ExperimentObject_list.rend(); expi++)
+  {
+   // Return the EXPID for every known experiment
+    cmd->Result_Int ((*expi)->ExperimentObject_ID());
+  }
+
+  cmd->set_Status(CMD_COMPLETE);
+  return true;
+}
+
+bool SS_ListHosts (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_ListObj (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_ListPids (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_ListMetrics (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_ListParams (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_ListReports (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_ListSrc (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_ListTypes (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+// Session Commands
+
+bool SS_ClearBreaks (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_Exit (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_Help (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_History (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+ // Default action with no arguments: Dump the history file to stdout
+  bool R = Command_Trace (cmd, CMDW_TRACE_ORIGINAL_COMMANDS, WindowID, std::string(""));
+
+  cmd->set_Status( R ? CMD_COMPLETE : CMD_ERROR);
+  return true;
+}
+
+bool SS_Log (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_OpenGui (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
+}
+
+bool SS_Playback (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
+
+  return true;
 }
 
 bool SS_Record (CommandObject *cmd) {
@@ -130,12 +405,22 @@ bool SS_Record (CommandObject *cmd) {
   char *tofile = NULL;
 
   if (tofile == NULL) {
-   (void)SpeedShop_Trace_OFF ();
+   (void)Command_Trace_OFF (WindowID);
   } else {
-    (void)SpeedShop_Trace_ON(tofile);
+    (void)Command_Trace_ON(WindowID, tofile);
   }
 
  // There is no result returned for this command.
+
+  return true;
+}
+
+bool SS_SetBreak (CommandObject *cmd) {
+  InputLineObject *clip = cmd->Clip();
+  CMDWID WindowID = (clip != NULL) ? clip->Who() : 0;
+
+  cmd->Result_String ("not yet implemented");
+  cmd->set_Status(CMD_COMPLETE);
 
   return true;
 }
