b6bc679a3fbfc6d8b3b02db7906b9d96148bf42e
stipek <>
2005-02-16 15:23:16 +0000

    Remove the (unused) accelerator.

--- Panels/pcSamplePanel/local_plugin_info.hxx
+++ Panels/pcSamplePanel/local_plugin_info.hxx
@@ -25,6 +25,7 @@
   plugin_entry->plugin_location = "$OPENSPEEDSHOP_PLUGIN_PATH";
   plugin_entry->plugin_entry_point = "panel_init";
   plugin_entry->menu_heading =  "E&xperiments";
-  plugin_entry->menu_label =  "&pc Sampling";
+//  plugin_entry->menu_label =  "&pc Sampling";
+  plugin_entry->menu_label =  "pc Sampling";
   plugin_entry->menu_accel =  "Alt+R";
   plugin_entry->panel_type =  "pc Sampling";
