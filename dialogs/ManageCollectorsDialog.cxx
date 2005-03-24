e16c6a8c946f6b6012141bc644797e29be944b48
stipek <>
2005-03-24 15:00:46 +0000

    Add the build references to ManageCollectorsDialog.*xx

---
+++ dialogs/ManageCollectorsDialog.cxx
@@ -0,0 +1,179 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2005 Silicon Graphics, Inc. All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+  
+
+#include "ManageCollectorsDialog.hxx"
+
+#include "debug.hxx"
+
+#include <qvariant.h>
+#include <qframe.h>
+#include <qpushbutton.h>
+#include <qlayout.h>
+#include <qtooltip.h>
+#include <qwhatsthis.h>
+#include <qimage.h>
+#include <qpixmap.h>
+#include <qlabel.h>
+#include <qcombobox.h>
+#include <qlistview.h>
+
+ManageCollectorsDialog::ManageCollectorsDialog( QWidget* parent, const char* name, bool modal, WFlags fl )
+    : QDialog( parent, name, modal, fl )
+{
+  nprintf(DEBUG_CONST_DESTRUCT) ("ManageCollectorsDialog::ManageCollectorsDialog() constructor called.\n");
+  
+  plo = NULL;
+  if ( !name ) setName( "ManageCollectorsDialog" );
+
+  setSizeGripEnabled( TRUE );
+  ManageCollectorsDialogLayout = new QVBoxLayout( this, 11, 6, "ManageCollectorsDialogLayout"); 
+
+  attachHostLabel = new QLabel( this, "attachHostLabel" );
+  ManageCollectorsDialogLayout->addWidget( attachHostLabel );
+  attachHostComboBox = new QComboBox( this, "attachHostComboBox");
+  attachHostComboBox->setSizePolicy( QSizePolicy( (QSizePolicy::SizeType)2, (QSizePolicy::SizeType)0, 0, 0, attachHostComboBox->sizePolicy().hasHeightForWidth() ) );
+  attachHostComboBox->setEditable(TRUE);
+  ManageCollectorsDialogLayout->addWidget( attachHostComboBox );
+
+  availableProcessListView = new QListView( this, "availableProcessListView" );
+  availableProcessListView->addColumn( tr( "Processes belonging to '%s':" ) );
+  availableProcessListView->setSelectionMode( QListView::Single );
+  availableProcessListView->setAllColumnsShowFocus( FALSE );
+  availableProcessListView->setShowSortIndicator( FALSE );
+  ManageCollectorsDialogLayout->addWidget( availableProcessListView );
+
+
+  Layout1 = new QHBoxLayout( 0, 0, 6, "Layout1"); 
+
+  buttonHelp = new QPushButton( this, "buttonHelp" );
+  buttonHelp->setAutoDefault( TRUE );
+  Layout1->addWidget( buttonHelp );
+  Horizontal_Spacing2 = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
+  Layout1->addItem( Horizontal_Spacing2 );
+
+  buttonOk = new QPushButton( this, "buttonOk" );
+  buttonOk->setAutoDefault( TRUE );
+  buttonOk->setDefault( TRUE );
+  Layout1->addWidget( buttonOk );
+
+  buttonCancel = new QPushButton( this, "buttonCancel" );
+  buttonCancel->setAutoDefault( TRUE );
+  Layout1->addWidget( buttonCancel );
+  ManageCollectorsDialogLayout->addLayout( Layout1 );
+  languageChange();
+  resize( QSize(511, 282).expandedTo(minimumSizeHint()) );
+  clearWState( WState_Polished );
+
+  // signals and slots connections
+  connect( buttonOk, SIGNAL( clicked() ), this, SLOT( accept() ) );
+  connect( buttonCancel, SIGNAL( clicked() ), this, SLOT( reject() ) );
+  connect( attachHostComboBox, SIGNAL( activated(const QString &) ), this, SLOT( attachHostComboBoxActivated() ) );
+
+  updateAttachableProcessList();
+}
+
+/*
+ *  Destroys the object and frees any allocated resources
+ */
+ManageCollectorsDialog::~ManageCollectorsDialog()
+{
+  // no need to delete child widgets, Qt does it all for us
+  nprintf(DEBUG_CONST_DESTRUCT) ("ManageCollectorsDialog::ManageCollectorsDialog() destructor called.\n");
+}
+
+/*
+ *  Sets the strings of the subwidgets using the current
+ *  language.
+ */
+void ManageCollectorsDialog::languageChange()
+{
+  setCaption( tr( "ManageCollectorsDialog" ) );
+  buttonHelp->setText( tr( "&Help" ) );
+  buttonHelp->setAccel( QKeySequence( tr( "F1" ) ) );
+  buttonOk->setText( tr( "&OK" ) );
+  buttonOk->setAccel( QKeySequence( QString::null ) );
+  buttonCancel->setText( tr( "&Cancel" ) );
+  buttonCancel->setAccel( QKeySequence( QString::null ) );
+  attachHostLabel->setText( tr("Host:") );
+  attachHostComboBox->insertItem( "localhost" );
+#ifdef LATER
+  attachHostComboBox->insertItem( "clink.americas.sgi.com" );
+  attachHostComboBox->insertItem( "hope.americas.sgi.com" );
+  attachHostComboBox->insertItem( "hope1.americas.sgi.com" );
+  attachHostComboBox->insertItem( "hope2.americas.sgi.com" );
+#endif // LATER
+}
+
+QString
+ManageCollectorsDialog::selectedProcesses()
+{
+//  QString ret_value = attachHostComboBox->currentText();
+
+  QListViewItem *selectedItem = availableProcessListView->selectedItem();
+  if( selectedItem )
+  {
+//    printf("Got an ITEM!\n");
+    QString ret_value = selectedItem->text(0);
+    return( ret_value );
+  } else
+  {
+//    printf("NO ITEMS SELECTED\n");
+    return( NULL );
+  }
+}
+
+
+
+void
+ManageCollectorsDialog::updateAttachableProcessList()
+{
+  char *host = (char *)attachHostComboBox->currentText().ascii();
+  ProcessEntry *pe = NULL;
+  char entry_buffer[1024];
+
+  if( plo )
+  {
+    delete(plo);
+  }
+//  printf("look up processes on host=(%s)\n", host);
+  plo = new ProcessListObject(host);
+
+  availableProcessListView->clear();
+  QListViewItem *item_2 = new QListViewItem( availableProcessListView, 0 );
+  item_2->setOpen( TRUE );
+  item_2->setText( 0, tr( "hostname" ) );
+
+  ProcessEntryList::Iterator it;
+  for( it = plo->processEntryList.begin();
+       it != plo->processEntryList.end();
+       ++it )
+  {
+    pe = (ProcessEntry *)*it;
+//    printf("%-20s %-10d %-20s\n", pe->host_name, pe->pid, pe->process_name);
+    sprintf(entry_buffer, "%-20s %-10d %-20s\n", pe->host_name, pe->pid, pe->process_name);
+    QListViewItem *item = new QListViewItem( item_2, 0 );
+    item->setText( 0, tr(entry_buffer) );
+  }
+}
+
+void ManageCollectorsDialog::attachHostComboBoxActivated()
+{
+//  printf("attachHostComboBoxActivated\n");
+    updateAttachableProcessList();
+}
---
+++ dialogs/ManageCollectorsDialog.hxx
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2005 Silicon Graphics, Inc. All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+  
+
+#ifndef ATTACHPROCESSDIALOG_H
+#define ATTACHPROCESSDIALOG_H
+
+#include <qvariant.h>
+#include <qdialog.h>
+
+#include "ProcessListObject.hxx"  // For getting pid list off a host...
+#include "ProcessEntryClass.hxx"
+
+class QVBoxLayout;
+class QHBoxLayout;
+class QGridLayout;
+class QSpacerItem;
+class QFrame;
+class QPushButton;
+class QLabel;
+class QComboBox;
+class QListView;
+class QListViewItem;
+
+class ManageCollectorsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ManageCollectorsDialog( QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0 );
+    ~ManageCollectorsDialog();
+
+    QPushButton* buttonHelp;
+    QPushButton* buttonOk;
+    QPushButton* buttonCancel;
+
+    QLabel* attachHostLabel;
+    QComboBox * attachHostComboBox;
+    QListView* availableProcessListView;
+
+    QString selectedProcesses();
+    void updateAttachableProcessList();
+
+    ProcessListObject *plo;
+
+protected:
+    QVBoxLayout* ManageCollectorsDialogLayout;
+    QHBoxLayout* Layout1;
+    QSpacerItem* Horizontal_Spacing2;
+
+protected slots:
+    virtual void languageChange();
+
+public slots:
+    virtual void attachHostComboBoxActivated();
+
+};
+
+#endif // ATTACHPROCESSDIALOG_H
