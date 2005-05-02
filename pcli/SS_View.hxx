e38e3256fb9decfaeded14bcc725f753f680c93a
dew <>
2005-05-02 21:34:50 +0000

    Alter argument definitions for generic view generation function.

--- pcli/SS_View.hxx
+++ pcli/SS_View.hxx
@@ -23,13 +23,21 @@ class ViewType
   std::string Brief_Description;  // What is the meaning of each column?
   std::string Short_Description;  // What type of report is generated?
   std::string Long_Description;   // When would I want to use this?
+  std::string *Metric_Name;       // This view requires these metrics.
   std::string *Collector_Name;    // This view requires these collectors.
   std::string *Headers_Name;      // The names associated with each column.
   bool Requires_Exp;              // This view is only meaninful with an experiment.
+  bool PreDetermine_Metrics;      // If false, the View will deterimne what is needed.
 
  public:
-  virtual bool GenerateView (CommandObject *cmd, ExperimentObject *exp, int64_t topn) {
-    cmd->Result_String ("The requested view is unavailable.");
+  // The call to generate the view must have this form.
+  // topn will be 0, if there is no integer on the right of the view name.
+  // useCollector will be a dummy string, if none is specified.
+  // virtual bool GenerateView (CommandObject *cmd, ExperimentObject *exp,
+  //                            int64_t topn, std::string useCollector) {
+  virtual bool GenerateView (CommandObject *cmd, ExperimentObject *exp, int64_t topn,
+                         ThreadGroup tgrp, std::vector<Collector> CV, std::vector<std::string> MV) {
+    cmd->Result_String ("The requested view has not been implemented.");
     cmd->set_Status(CMD_ERROR);
     return false;
   }
@@ -42,31 +50,60 @@ class ViewType
             std::string BriefD,
             std::string ShortD,
             std::string LongD,
+            std::string *Metric,
             std::string *Collect,
             std::string *Head,
-            bool NeedsExp = true) {
+            bool NeedsExp = true,
+            bool DeterimneMetrics = true) {
     Id = viewname;
-    Brief_Description = BriefD;
-    Short_Description = ShortD;
-    Long_Description  = LongD;
-    Collector_Name    = Collect;
-    Headers_Name      = Head;
-    Requires_Exp      = NeedsExp;
+    Brief_Description    = BriefD;
+    Short_Description    = ShortD;
+    Long_Description     = LongD;
+    Metric_Name          = Metric;
+    Collector_Name       = Collect;
+    Headers_Name         = Head;
+    Requires_Exp         = NeedsExp;
+    PreDetermine_Metrics = DeterimneMetrics;
   }
   std::string Unique_Name() { return Id; }
   std::string Brief_Name() { return Brief_Description; }
   std::string Short_Name() { return Short_Description; }
   std::string Long_Name() { return Long_Description; }
+  std::string *Metrics() { return Metric_Name; }
   std::string *Collectors() { return Collector_Name; }
   std::string *Headers() { return Headers_Name; }
   bool        Need_Exp() { return Requires_Exp; }
+  bool        Deterimne_Metrics() { return PreDetermine_Metrics; }
 };
 
-extern std::list<ViewType *> Available_Views;
-ViewType *Find_View (std::string viewname);
-void Add_Header (CommandObject *cmd, std::string *column_titles);
 
+extern std::list<ViewType *> Available_Views;
+extern ViewType *Generic_View;
 inline void Define_New_View (ViewType *vnew) { Available_Views.push_front (vnew); }
+
+typedef std::pair<Function, double> Function_double_pair;
+CommandResult *Get_Collector_Metric (CommandObject *cmd,
+                                     Function F,
+                                     ThreadGroup tgrp,
+                                     Collector collector,
+                                     std::string metric);
+std::vector<Function_double_pair>
+                 GetDoubleByFunction (CommandObject *cmd,
+                                      bool ascending_sort,
+                                      ThreadGroup tgrp,
+                                      Collector C,
+                                      std::string metric);
+double Total_second (std::vector<Function_double_pair> items);
+ViewType *Find_View (std::string viewname);
+std::string Find_Collector_With_Metrics (CollectorGroup cgrp,
+                                         std::string *Metric_List);
+bool Collector_Generates_Metrics (Collector C, std::string *Metric_List);
+bool Metadata_hasName (std::set<Metadata> M, std::string name);
+Metadata Find_Metadata (Collector C, std::string name);
+CommandResult *gen_F_name (Function F);
+
+void Add_Column_Headers (CommandResult_Headers *H, std::string *column_titles);
+void Add_Header (CommandObject *cmd, std::string *column_titles);
 
 void SS_Init_BuiltIn_Views ();
 void SS_Get_Views (CommandObject *cmd);
