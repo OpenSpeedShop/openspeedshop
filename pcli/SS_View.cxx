4e4db591760d4cb55615effedaf7806ba73c547e
dew <>
2005-05-02 21:34:49 +0000

    Modify Collector and Metric determination.

--- pcli/SS_View.cxx
+++ pcli/SS_View.cxx
@@ -30,18 +30,11 @@ using namespace OpenSpeedShop::cli;
 
 // Global data definition
 std::list<ViewType *> Available_Views;
+ViewType *Generic_View = NULL;
 
-// View management utilities
+// Local utilities
 
-static bool Reaminaing_Length_Is_Numeric (std::string viewname, int64_t L) {
-  for (int64_t i = L; i < viewname.length(); i++) {
-    char a = viewname[i];
-    if ((a < *"0") || (a > *"9")) return false;
-  }
-  return true;
-}
-
-static int64_t Get_Traling_Int (std::string viewname, int64_t start) {
+static int64_t Get_Trailing_Int (std::string viewname, int64_t start) {
   int64_t topn = 0;
   for (int i = start; i < viewname.length(); i++) {
     char c = viewname[i];
@@ -50,31 +43,6 @@ static int64_t Get_Traling_Int (std::string viewname, int64_t start) {
     }
   }
   return topn;
-}
- 
-ViewType *Find_View (std::string viewname) {
-  std::list<ViewType *>::iterator vi;
-  for (vi = Available_Views.begin(); vi != Available_Views.end(); vi++) {
-    ViewType *vt = (*vi);
-    if (!strncasecmp (viewname.c_str(), vt->Unique_Name().c_str(), vt->Unique_Name().length()) &&
-        Reaminaing_Length_Is_Numeric (viewname, vt->Unique_Name().length())) {
-      return vt;
-    }
-  }
-
-  return NULL;
-}
-
-void Add_Header (CommandObject *cmd, std::string *column_titles)
-{
-  CommandResult_Headers *H = new CommandResult_Headers ();
-  int64_t i = 0;
-  while (column_titles[i].length() != 0) {
-    CommandResult *T = new CommandResult_String ( column_titles[i] );
-    H->CommandResult_Headers::Add_Header (T);
-    i++;
-  }
-  cmd->Result_Predefined (H);
 }
 
 // Supporting semantic routines
@@ -89,17 +57,51 @@ void SS_Get_Views (CommandObject *cmd) {
 }
 
 void SS_Get_Views (CommandObject *cmd, std::string collector_name) {
- // List all views that require a specific colelctor.
+ // List all views that require a specific collector.
   std::list<ViewType *>::iterator vi;
   for (vi = Available_Views.begin(); vi != Available_Views.end(); vi++) {
     ViewType *vt = (*vi);
     std::string *Collector_List = vt->Collectors();
-    std::string C = Collector_List[0];
-    while (C.length() != 0) {
-      if ( !strcasecmp (C.c_str(), collector_name.c_str()) ) {
-        cmd->Result_String ( vt->Unique_Name() );
+    std::string *Metric_List = vt->Metrics();
+    bool all_collectors_available = false;
+
+    if (Collector_List[0].length() > 0) {
+     // Check that all the required collectors are available.
+      std::string cs = Collector_List[0];
+      all_collectors_available = true;
+
+      while (cs.length() != 0) {
+        if ( strcasecmp (cs.c_str(), collector_name.c_str()) ) {
+          all_collectors_available = false;
+          break;
+        }
+        cs = *(++Collector_List);
       }
-      C = *(++Collector_List);
+
+    }
+
+    if (!all_collectors_available &&
+        (Metric_List[0].length() > 0)) {
+     // Look for an available collector that produces the required metrics.
+      try {
+        std::string tmpdb = std::string("./ssdbtmpview.openss");
+        OpenSpeedShop::Framework::Experiment::create (tmpdb);
+        OpenSpeedShop::Framework::Experiment *fw_exp = new OpenSpeedShop::Framework::Experiment (tmpdb);
+        Collector C = Get_Collector (fw_exp, collector_name);
+        if (Collector_Generates_Metrics ( C, &Metric_List[0] )) {
+          all_collectors_available = true;
+        }
+        delete fw_exp;
+        (void) remove (tmpdb.c_str());
+      }
+      catch(const Exception& error) {
+       // Guess not.
+        all_collectors_available = false;
+      }
+    }
+
+    if (all_collectors_available) {
+        cmd->Result_String ( vt->Unique_Name() );
     }
   }
   
@@ -110,58 +112,85 @@ void SS_Get_Views (CommandObject *cmd, OpenSpeedShop::Framework::Experiment *fex
   CollectorGroup cgrp;
   try {
     cgrp = fexp->getCollectors();
+
+    std::list<ViewType *>::iterator vi;
+    for (vi = Available_Views.begin(); vi != Available_Views.end(); vi++) {
+     // All the required collectors must exist in the CollectorGroup.
+      ViewType *vt = (*vi);
+      std::string *Collector_List = vt->Collectors();
+      std::string *Metric_List = vt->Metrics();
+      bool all_collectors_available = ((Collector_List[0].length() == 0) &&
+                                       (Metric_List[0].length() == 0));
+
+      if (!all_collectors_available &&
+          (Collector_List[0].length() > 0)) {
+       // Check that all the required collectors are available.
+        std::string C = Collector_List[0];
+        all_collectors_available = true;
+
+        while (C.length() != 0) {
+          bool required_collector_found = false;
+          CollectorGroup::iterator ci;
+
+          for (ci = cgrp.begin(); ci != cgrp.end(); ci++) {
+            Collector c = *ci;
+            Metadata m = c.getMetadata();
+            std::string collector_name = m.getUniqueId();
+            if ( !strcasecmp (C.c_str(), collector_name.c_str()) ) {
+              required_collector_found = true;
+              break;
+            }
+          }
+
+          if (required_collector_found) {
+            C = *(++Collector_List);
+            continue;
+          } else {
+            all_collectors_available = false;
+            break;
+          }
+        }
+
+      }
+
+      if (!all_collectors_available &&
+          (Metric_List[0].length() > 0)) {
+       // Look for an available collector that produces the required metrics.
+        CollectorGroup::iterator ci;
+
+        for (ci = cgrp.begin(); ci != cgrp.end(); ci++) {
+          Collector C = *ci;
+          if (Collector_Generates_Metrics ( C, &Metric_List[0] )) {
+            all_collectors_available = true;
+            break;
+          }
+        }
+
+      }
+
+      if (all_collectors_available) {
+        cmd->Result_String ( vt->Unique_Name() );
+      }
+    }
   }
   catch (const Exception& error) {
     Mark_Cmd_With_Std_Error (cmd, error);
     return;
-  }
-
-  std::list<ViewType *>::iterator vi;
-  for (vi = Available_Views.begin(); vi != Available_Views.end(); vi++) {
-   // All the required collectors must exist in the CollectorGroup.
-    bool all_collectors_available = true;
-    ViewType *vt = (*vi);
-    std::string *Collector_List = vt->Collectors();
-    std::string C = Collector_List[0];
-
-    while (C.length() != 0) {
-      bool required_collector_found = false;
-      CollectorGroup::iterator ci;
-
-      for (ci = cgrp.begin(); ci != cgrp.end(); ci++) {
-        Collector c = *ci;
-        Metadata m = c.getMetadata();
-        std::string collector_name = m.getUniqueId();
-        if ( !strcasecmp (C.c_str(), collector_name.c_str()) ) {
-          required_collector_found = true;
-          break;
-        }
-      }
-
-      if (required_collector_found) {
-        C = *(++Collector_List);
-        continue;
-      } else {
-        all_collectors_available = false;
-        break;
-      }
-    }
-
-    if (all_collectors_available) {
-      cmd->Result_String ( vt->Unique_Name() );
-    }
   }
   
 }
 
 bool SS_Generate_View (CommandObject *cmd, ExperimentObject *exp, std::string viewname) {
 
+ // Determine the availability of the view.
   ViewType *vt = Find_View (viewname);
   if (vt == NULL) {
     cmd->Result_String ("The requested view is unavailable.");
     cmd->set_Status(CMD_ERROR);
     return false;
   }
+
+ // Check for a required experiment.
   if (vt->Need_Exp()) {
     if (exp == NULL) {
      // The requested view requires an ExperimentObject.
@@ -176,16 +205,79 @@ bool SS_Generate_View (CommandObject *cmd, ExperimentObject *exp, std::string vi
     }
   }
 
-  return vt->GenerateView (cmd, exp, Get_Traling_Int (viewname, vt->Unique_Name().length()));
+ // Determine the required Collector.
+  bool collector_found = false;
+  std::string *Metric_List = vt->Metrics();
+  std::string *Collector_List = vt->Collectors();
+  std::string C_Name = Collector_List[0];
+  if ((Collector_List[0].length() != 0) &&
+      (Collector_List[1].length() == 0)) {
+   // There is a single, required collector.
+     C_Name = vt->Unique_Name();
+     collector_found = true;
+  } else if (!collector_found &&
+             (Metric_List[0].length() > 0) &&
+             (exp != NULL) &&
+             (exp->FW() != NULL)) {
+   // Look for a collector that produces the required metrics.
+    CollectorGroup cgrp = exp->FW()->getCollectors();
+    C_Name = Find_Collector_With_Metrics (cgrp, &Metric_List[0]);
+    collector_found = (C_Name.length() > 0);
+  } else {
+   // We handle multiple collectors by letting the View figure out what to do.
+   // This also includes Views that require no collectors.
+  }
+
+  if (vt->Need_Exp() &&
+      ((Collector_List[0].length() != 0) ||
+       (Metric_List[0].length() != 0))) {
+    if (!collector_found) {
+      cmd->Result_String ("The metrics required to generate the view are not available in the experiment.");
+      cmd->set_Status(CMD_ERROR);
+      return false;
+    }
+    if (!Collector_Used_In_Experiment (exp->FW(), C_Name)) {
+      cmd->Result_String ("The required collector, " + C_Name + ", was not used in the experiment.");
+      cmd->set_Status(CMD_ERROR);
+      return false;
+    }
+  }
+
+ // Determine threads, collectors and metrics that are required.
+  ThreadGroup tgrp;
+  std::vector<Collector> CV;
+  std::vector<std::string> MV;
+  if (vt->Deterimne_Metrics()) {
+    tgrp = exp->FW()->getThreads();
+    CollectorGroup cgrp = exp->FW()->getCollectors();
+    CollectorGroup::iterator ci;
+    for (ci = cgrp.begin(); ci != cgrp.end(); ci++) {
+      Collector c = *ci;
+      Metadata cm = c.getMetadata();
+      std::set<Metadata> md = c.getMetrics();
+      std::set<Metadata>::const_iterator mi;
+      for (mi = md.begin(); mi != md.end(); mi++) {
+        Metadata m = *mi;
+        CV.push_back(c);
+        MV.push_back(m.getUniqueId());
+      }
+    }
+  }
+
+ // Try to Generate the Requested View!
+  return vt->GenerateView (cmd, exp, Get_Trailing_Int (viewname, vt->Unique_Name().length()),
+                           tgrp, CV, MV);
 }
 
 // Initialize definitions of the predefined views.
 extern "C" void exp_LTX_ViewFactory ();
 extern "C" void pcfunc_LTX_ViewFactory ();
 extern "C" void params_LTX_ViewFactory ();
+extern "C" void stats_LTX_ViewFactory ();
 
 void SS_Init_BuiltIn_Views () {
   exp_LTX_ViewFactory ();
+  stats_LTX_ViewFactory ();
   pcfunc_LTX_ViewFactory ();
   params_LTX_ViewFactory ();
 }
