8ff1e599f7a9fd9c10d5d981ae8862b9c9845a96
jcarter <>
2005-06-06 23:53:53 +0000

    Put in support for expView.

--- pcli/parse_test/input.cxx
+++ pcli/parse_test/input.cxx
@@ -236,6 +236,21 @@ arglist_t view_arg[VIEW_MAX] = {
 //*************************************************************
 
 
+char *metric_list[] = {
+    NULL,
+    "pcsamp::view1",
+    "pcsamp::view1,pcsamp::view2,exptype2::view2",
+    "exptype3"
+};
+
+#define METRIC_MAX 1
+arglist_t metric_arg[METRIC_MAX] = {
+    4,metric_list,NULL
+};
+
+//*************************************************************
+
+
 char *lineno_list[] = {
     NULL,
     "27",
@@ -419,6 +434,75 @@ three_level(char *cmd_name,
 }
 
 /**
+ * Function: three_level
+ * 
+ * triple loop.
+ *     
+ * @param   xx    	xx.
+ *
+ * @return  void.
+ *
+ */
+void
+five_level(char *cmd_name,
+    	    int start_1,int max_1,arglist_t *p_arglist_1,char *arg_wrap_1,
+    	    int start_2,int max_2,arglist_t *p_arglist_2,char *arg_wrap_2,
+    	    int start_3,int max_3,arglist_t *p_arglist_3,char *arg_wrap_3,
+    	    int start_4,int max_4,arglist_t *p_arglist_4,char *arg_wrap_4,
+    	    int start_5,int max_5,arglist_t *p_arglist_5,char *arg_wrap_5
+	    )
+{
+    int i1,i2,j1,j2,k1,k2,l1,l2,m1,m2;
+    
+    // 
+    for (i1=0; i1<max_1; ++i1) {
+    	for (i2=start_1; i2<p_arglist_1[i1].arg_count; ++i2) {
+	    char *p_arg_1 = p_arglist_1[i1].arg_strings[i2];
+    	    for (j1=0; j1<max_2; ++j1) {
+	    	for (j2=start_2; j2<p_arglist_2[j1].arg_count; j2++) {
+		    char *p_arg_2 = p_arglist_2[j1].arg_strings[j2];
+		    for (k1=0; k1<max_3; ++k1) {
+		    	 for (k2=start_3; k2<p_arglist_3[k1].arg_count; k2++) {
+			    char *p_arg_3 = p_arglist_3[k1].arg_strings[k2];
+		    	    for (l1=0; l1<max_4; ++l1) {
+		    	    	for (l2=start_4; l2<p_arglist_4[l1].arg_count; l2++) {
+			    	    char *p_arg_4 = p_arglist_4[l1].arg_strings[l2];
+		    	    	    for (m1=0; m1<max_5; ++m1) {
+		    	    	    	for (m2=start_5; m2<p_arglist_5[m1].arg_count; m2++) {
+			    	    	    char *p_arg_5 = p_arglist_5[m1].arg_strings[m2];
+
+	    	    	    	    	    out_stream << cmd_name;
+
+	    	    	    	    	    if (p_arg_1)
+			    	    	    	print_arg(p_arglist_1[i1].identifier,p_arg_1);
+	    	    	    	    	    if (p_arg_2)
+			    	    	    	print_arg(p_arglist_2[j1].identifier,p_arg_2);
+	    	    	    	    	    if (p_arg_3)
+			    	    	    	print_arg(p_arglist_3[k1].identifier,p_arg_3);
+	    	    	    	    	    if (p_arg_4) {
+					    	if (arg_wrap_4)
+						    out_stream << " " << arg_wrap_4[0] << " ";
+			    	    	    	print_arg(p_arglist_4[l1].identifier,p_arg_4);
+					    	if (arg_wrap_4)
+						    out_stream << " " << arg_wrap_4[1] << " ";
+					    }
+	    	    	    	    	    if (p_arg_5)
+			    	    	    	print_arg(p_arglist_5[m1].identifier,p_arg_5);
+
+		    	    	    	    out_stream << endl;
+					}
+				    }
+				}
+			    }
+			}
+		    }
+	    	}
+	    }
+	}
+    }
+}
+
+/**
  * Function: open_output
  * 
  * triple loop.
@@ -581,10 +665,12 @@ main()
 
     // EXPVIEW
     p_os = open_output("expview.input");
-    three_level("expview",
-    	    	0,EXPID_MAX,exp_id_arg,
-    	    	0,GUI_MAX,gui_arg,
-    	    	1,VIEW_MAX,view_arg
+    five_level ("expview",
+    	    	0,EXPID_MAX,exp_id_arg,NULL,
+    	    	0,GUI_MAX,gui_arg,NULL,
+    	    	1,VIEW_MAX,view_arg,NULL,
+    	    	0,METRIC_MAX,metric_arg,"()",
+    	    	0,TARGET_MAX,target_arg,NULL
     	    	);
 
     dunp_close_output(p_os);
