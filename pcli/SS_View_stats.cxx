ddafad16cd45fb0a63f36b718b205f448fe9ed22
dew <>
2005-06-09 20:22:39 +0000

    Expand generic view to process ViewInstruction facility.

--- pcli/SS_View_stats.cxx
+++ pcli/SS_View_stats.cxx
@@ -48,7 +48,10 @@ static std::string VIEW_pcfunc_header[] =
   { ""
   };
 static bool VIEW_stats (CommandObject *cmd, ExperimentObject *exp, int64_t topn,
-                        ThreadGroup tgrp, std::vector<Collector> CV, std::vector<std::string> MV) {
+                        ThreadGroup tgrp, std::vector<Collector> CV, std::vector<std::string> MV,
+                        std::vector<ViewInstruction *>IV) {
+  // Print_View_Params (stderr, CV,MV,IV);
+
   bool report_Column_summary = false;
   std::vector<CommandResult *> Column_Sum;
 
@@ -56,52 +59,107 @@ static bool VIEW_stats (CommandObject *cmd, ExperimentObject *exp, int64_t topn,
 
   try {
     if (CV.size() == 0) {
+      cmd->Result_String ("(There are no metrics specified to report.)");
+      cmd->set_Status(CMD_ERROR);
       return false;   // There is no collector, return.
     }
-    std::vector<Function_double_pair> items = GetDoubleByFunction (cmd, false, tgrp, CV[0], MV[0]);
+    ViewInstruction *baseInst = Find_Base_Def (IV);
+    int64_t baseIndex = 0;
+    if (baseInst != NULL) {
+      baseIndex = baseInst->TMP1();
+    }
+    ViewInstruction *totalInst = Find_Total_Def (IV);
+    if (totalInst == NULL) {
+      totalInst = Find_Percent_Def (IV);
+    }
+    bool Gen_Total_Percent = (totalInst != NULL);
+    int64_t totalIndex = 0;
+    if (Gen_Total_Percent) {
+      totalIndex = totalInst->TMP1();
+      if (baseInst == NULL) {
+        baseIndex = totalIndex;
+      }
+    }
+    std::vector<Function_double_pair> items = GetDoubleByFunction (cmd, false, tgrp, CV[baseIndex], MV[baseIndex]);
     if (items.begin() == items.end()) {
+      cmd->Result_String ("(There are no data samples for " + MV[baseIndex] + " available.)");
+      cmd->set_Status(CMD_ERROR);
       return false;   // There is no data, return.
     }
 
    // Calculate %?
-    bool Gen_Total_Percent = (CV.size() == 1);
     CommandResult *TotalValue = NULL;
     if (Gen_Total_Percent) {
+     // We calculate Total Time by adding all the time in the base metric.
+     // Unfortunately, there may be time intervals that are not included
+     // in the base metric, so the total we calcualte may be less than the
+     // actual total.  Worst yet, it ma be less than the total for an item
+     // displayed in Column[0] leading to a % reported that is over 100.
+      TotalValue = new CommandResult_Float();
+
+      std::vector<Function_double_pair>::const_iterator itt = items.begin();
+      for( ; itt != items.end(); itt++ ) {
+        Function_double_pair f = *itt;
+        CommandResult *Metric_Value = Get_Collector_Metric( cmd, f.first, tgrp,
+                                                            CV[totalIndex], MV[totalIndex] );
+        Accumulate_CommandResult (TotalValue, Metric_Value);
+      }
+
       double TotalTime = 0.0;
-      TotalTime = Total_second (items);
+      ((CommandResult_Float *)TotalValue)->Value(TotalTime);
       if (TotalTime < 0.0000000001) {
-        cmd->Result_String ( "(the measured time interval is too small)" );
+        cmd->Result_String ( "(The measured time interval is too small.)" );
         cmd->set_Status(CMD_ERROR);
         return false;
       }
-      TotalValue = new CommandResult_Float (TotalTime);
     }
 
    // Build a Header for the table.
     CommandResult_Headers *H = new CommandResult_Headers ();
    // Add Metrics
     int64_t i;
-    for ( i=0; i < MV.size(); i++) {
-      Metadata m = Find_Metadata ( CV[i], MV[i] );
-      H->CommandResult_Headers::Add_Header ( new CommandResult_String ( m.getShortName() ) );
+    for ( i=0; i < IV.size(); i++) {
+      ViewInstruction *vinst = Find_Column_Def (IV, i);
+      if (vinst == NULL) {
+       // Exit if we didn't find a definition
+        break;
+      }
+      int64_t CM_Index = vinst->TMP1();
+
+      std::string column_header;
+      if (vinst->OpCode() == VIEWINST_Display_Metric) {
+        Metadata m = Find_Metadata ( CV[CM_Index], MV[CM_Index] );
+        column_header = m.getShortName();
+      } else if (vinst->OpCode() == VIEWINST_Display_Tmp) {
+        column_header = std::string("Temp" + CM_Index);
+      } else if ((vinst->OpCode() == VIEWINST_Display_Percent_Column) ||
+                 (vinst->OpCode() == VIEWINST_Display_Percent_Metric) ||
+                 (vinst->OpCode() == VIEWINST_Display_Percent_Tmp)) {
+        column_header = "% of Total";
+      }
+      H->CommandResult_Headers::Add_Header ( new CommandResult_String ( column_header ) );
       if (report_Column_summary) {
         CommandResult *S = new CommandResult_Float();
         Column_Sum.push_back(S);
       }
-      if ((i == 0) && Gen_Total_Percent) {
-        H->CommandResult_Headers::Add_Header ( new CommandResult_String ( "% of Total" ) );
-        if (report_Column_summary) {
-          CommandResult *S = new CommandResult_Float();
-          Column_Sum.push_back(S);
-        }
-      }
     }
    // Add Function
-    H->CommandResult_Headers::Add_Header ( new CommandResult_String ( "Name" ) );
+    H->CommandResult_Headers::Add_Header ( new CommandResult_String ( "Function Name" ) );
     if (report_Column_summary) {
       Column_Sum.push_back(NULL);
     }
     cmd->Result_Predefined (H);
+
+   // Be sure we sort the items based on the metric displayed in the first column.
+    ViewInstruction *vinst0 = Find_Column_Def (IV, 0);
+    if (vinst0 != NULL) {
+      int64_t Column0index = vinst0->TMP1();
+      if ((Column0index != baseIndex) &&
+          ((CV[Column0index] != CV[baseIndex]) ||
+           (MV[Column0index] != MV[baseIndex]))) {
+        items = GetDoubleByFunction (cmd, false, tgrp, CV[Column0index], MV[Column0index]);
+      }
+    }
 
    // Extract the top "n" items from the sorted list.
     std::vector<Function_double_pair>::const_iterator it = items.begin();
@@ -110,23 +168,35 @@ static bool VIEW_stats (CommandObject *cmd, ExperimentObject *exp, int64_t topn,
       CommandResult_Columns *C = new CommandResult_Columns ();
 
      // Add Metrics
-      CommandResult *Metric_Result = new CommandResult_Float (it->second);
-      C->CommandResult_Columns::Add_Column (Metric_Result);
-      if (report_Column_summary) {
-        Accumulate_CommandResult (Column_Sum[0], Metric_Result);
-      }
-      if (Gen_Total_Percent) {
-        CommandResult *P = Calculate_Percent (Metric_Result, TotalValue);
-        C->CommandResult_Columns::Add_Column (P);
-        if (report_Column_summary) {
-          Accumulate_CommandResult (Column_Sum[1], P);
+      for ( i=0; i < IV.size(); i++) {
+        ViewInstruction *vinst = Find_Column_Def (IV, i);
+        if (vinst == NULL) {
+         // Exit if we didn't find a definition
+          break;
         }
-      }
-      for ( i=1; i < MV.size(); i++) {
-        CommandResult *Next_Metric_Value  = Get_Collector_Metric( cmd, it->first, tgrp, CV[i], MV[i] );
+        int64_t CM_Index = vinst->TMP1();
+
+        CommandResult *Next_Metric_Value = NULL;
+        if (vinst->OpCode() == VIEWINST_Display_Metric) {
+          Next_Metric_Value = Get_Collector_Metric( cmd, it->first, tgrp,
+                                                    CV[CM_Index], MV[CM_Index] );
+        } else if (vinst->OpCode() == VIEWINST_Display_Tmp) {
+          // Next_Metric_Value  = ???
+        } else if (vinst->OpCode() == VIEWINST_Display_Percent_Column) {
+          ViewInstruction *percentInst = Find_Column_Def (IV, vinst->TMP1());
+          int64_t percentIndex = percentInst->TMP1();
+          CommandResult *Metric_Result = Get_Collector_Metric( cmd, it->first, tgrp,
+                                                               CV[percentIndex], MV[percentIndex] );
+          Next_Metric_Value = Calculate_Percent (Metric_Result, TotalValue);
+        } else if ((vinst->OpCode() == VIEWINST_Display_Percent_Metric) ||
+                   (vinst->OpCode() == VIEWINST_Display_Percent_Tmp)) {
+          CommandResult *Metric_Result = Get_Collector_Metric( cmd, it->first, tgrp,
+                                                               CV[CM_Index], MV[CM_Index] );
+          Next_Metric_Value = Calculate_Percent (Metric_Result, TotalValue);
+        }
         C->CommandResult_Columns::Add_Column (Next_Metric_Value);
         if (report_Column_summary) {
-          Accumulate_CommandResult (Column_Sum[i+(Gen_Total_Percent?1:0)], Next_Metric_Value);
+          Accumulate_CommandResult (Column_Sum[i], Next_Metric_Value);
         }
       }
      // Add ID for row
@@ -172,8 +242,9 @@ class stats_view : public ViewType {
                            true) {
   }
   virtual bool GenerateView (CommandObject *cmd, ExperimentObject *exp, int64_t topn,
-                         ThreadGroup tgrp, std::vector<Collector> CV, std::vector<std::string> MV) {
-    return VIEW_stats (cmd, exp, topn, tgrp, CV, MV);
+                         ThreadGroup tgrp, std::vector<Collector> CV, std::vector<std::string> MV,
+                         std::vector<ViewInstruction *>IV) {
+    return VIEW_stats (cmd, exp, topn, tgrp, CV, MV, IV);
   }
 };
 
