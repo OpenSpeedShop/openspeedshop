1d8e32fd30555983dca4e13f58a3ed799f01dd95
helens <>
2005-08-11 17:33:23 +0000

    No Message Supplied

---
+++ test/executables/forever_cxx/forever.cxx
@@ -0,0 +1,62 @@
+/* A program that never finishes */
+#include <iostream>
+
+using namespace std;
+void frank()
+{
+  int i, j;
+  int t = 0;
+
+  for(i=0; i<1000; i++)
+  {
+    for(j=0; j<1000; j++)
+    {
+      t += i*j;
+    }
+  }
+}
+
+void foo()
+{
+  int i, j;
+  int t = 0;
+
+  for(i=0; i<1000; i++)
+  {
+    for(j=0; j<1000; j++)
+    {
+      t += i*j;
+    }
+  }
+}
+
+void bill()
+{
+  int i, j, k;
+  int t = 0;
+
+  for(i=0; i<100; i++)
+  {
+    for(j=0; j<100; j++)
+    {
+      for(k=0; k<1000; k++)
+      {
+        t += i*j*k;
+      }
+    }
+  }
+}
+
+main()
+{
+  int i;
+  int number = 50000;
+  cout << getpid() << endl;
+
+  for(i=0;i<number;i++)
+    {
+      foo();
+      frank();
+      bill();
+    }
+}
