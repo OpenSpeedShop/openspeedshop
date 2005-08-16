4ac6b567b6270c8b0810a878ba4fb2ec40d8130e
jcarter <>
2005-08-16 17:16:32 +0000

    Added more tests.

--- pcli/scripting/test.py
+++ pcli/scripting/test.py
@@ -40,14 +40,15 @@ oss.RankList(my_rank).add(44)
 # on IRIX and Linux. I'm going to change the
 # constructor to take a variable number of
 # arguments and check for len value of 0.
-my_rank2 = oss.RankList([])
+#my_rank2 = oss.RankList([])
+my_rank2 = oss.RankList()
 my_rank2.add(1234)
 my_rank2.add(34)
 my_rank2 += 888
 
 # The number here is hard coded in a lowlevel routine
-my_id = oss.expCreate(my_host,my_rank2,my_rank);
-print my_id
+my_id = oss.expCreate(my_host,my_rank2,my_rank)
+print "my_id: ", my_id
 
 # I need to identify the argument as an experiment
 # id so you need to get it into an ExpId object.
@@ -55,5 +56,35 @@ exp1 = oss.ExpId(my_id)
 oss.expGo(exp1)
 
 oss.expView(exp1,"vtop88")
+
+my_cluster = oss.ClusterList("cluster_1")
+my_cluster.add(["cluster_2"])
+my_cluster += ["cluster_3","cluster_4"]
+
+my_id = oss.expCreate(my_cluster)
+
+my_thread = oss.ThreadList(987)
+my_thread += 765
+my_thread.add((22,33))
+
+my_id = oss.expCreate(my_thread)
+
+my_file = oss.FileList("file_1")
+my_file.add(["file_2"])
+my_file += ["file_3","file_4"]
+
+my_id = oss.expCreate(my_file)
+
+my_pid = oss.PidList(387)
+my_pid += 2600
+my_pid.add((1,25))
+
+my_id = oss.expCreate(my_pid)
+
+my_line = oss.LineList(22222)
+my_line += 222999
+my_line.add((333,444))
+
+my_id = oss.expCreate(my_line)
 
 
