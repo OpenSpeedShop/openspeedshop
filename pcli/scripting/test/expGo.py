b465acc7f6ce6d84d251764f1d06cc51bda97f70
jcarter <>
2005-08-23 16:51:54 +0000

    Took out example of both expId and -v all. I'm not ready for negative testing yet.

--- pcli/scripting/test/expGo.py
+++ pcli/scripting/test/expGo.py
@@ -6,7 +6,7 @@ exp1 = oss.ExpId(7)
 
 my_modifer = oss.ModifierList("all")
 
-oss.expGo(exp1,my_modifer)
+#oss.expGo(exp1,my_modifer)
 
 oss.expGo(my_modifer)
 
--- pcli/scripting/test/expPause.py
+++ pcli/scripting/test/expPause.py
@@ -6,7 +6,7 @@ exp1 = oss.ExpId(7)
 
 my_modifer = oss.ModifierList("all")
 
-oss.expPause(exp1,my_modifer)
+#oss.expPause(exp1,my_modifer)
 
 oss.expPause(my_modifer)
 
