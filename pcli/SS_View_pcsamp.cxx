da787ffa5810686984f71944e51865267aa6954d
dew <>
2005-09-07 19:22:13 +0000

    Add a view just for hwctime.

--- pcli/SS_View_pcsamp.cxx
+++ pcli/SS_View_pcsamp.cxx
@@ -216,6 +216,109 @@ class hwc_view : public ViewType {
   }
 };
 
+// Hardware Counter UserTime Report
+
+static std::string VIEW_hwctime_brief = "Usertime call stack report";
+static std::string VIEW_hwctime_short = "Report the amount of time spent in a function.";
+static std::string VIEW_hwctime_long  = "The report is sorted in descending order by the amount of time that"
+                                       " was used in each function.  Included in the report is both the time"
+                                       " used individually by each function (exclusive_time) and the time used"
+                                       " in aggregate by the function and all the functions it calls"
+                                       " (inclusive_time).  Also included in the report is the" 
+                                       " percent of total time that each function uses."
+                                       " A positive integer can be added to the end of the keyword"
+                                       " 'hwctime' to indicate the maximum number of items in"
+                                       " the report.";
+static std::string VIEW_hwctime_metrics[] =
+  { "inclusive_overflows",
+    "exclusive_overflows",
+    ""
+  };
+static std::string VIEW_hwctime_collectors[] =
+  { "hwctime",
+    ""
+  };
+static std::string VIEW_hwctime_header[] =
+  { ""
+  };
+class hwctime_view : public ViewType {
+
+ public: 
+  hwctime_view() : ViewType ("hwctime",
+                             VIEW_hwctime_brief,
+                             VIEW_hwctime_short,
+                             VIEW_hwctime_long,
+                            &VIEW_hwctime_metrics[0],
+                            &VIEW_hwctime_collectors[0],
+                            &VIEW_hwctime_header[0],
+                             true,
+                             false) {
+  }
+  virtual bool GenerateView (CommandObject *cmd, ExperimentObject *exp, int64_t topn,
+                         ThreadGroup tgrp, std::vector<Collector> CV, std::vector<std::string> MV,
+                         std::vector<ViewInstruction *>IV) {
+    if (MV.size() > 2) {
+     // There are only two metrics.  Default to the basic report.
+      CV.erase(CV.begin(), CV.end());
+      MV.erase(MV.begin(), MV.end());
+      IV.erase(IV.begin(), IV.end());
+    }
+
+    int64_t Max_Column = Find_Max_Column_Def (IV);
+
+   // The user might have already define the metrics, but it doesn't hurt to do it again.
+    Collector c = Get_Collector (exp->FW(), VIEW_hwctime_collectors[0]);
+
+   // Define inclusive time metric.
+    int intime_index = MV.size();
+    CV.push_back (c);
+    MV.push_back ("inclusive_overflows");
+
+   // Define exclusive time metric.
+    int extime_index = intime_index + 1;
+    CV.push_back (c);
+    MV.push_back ("exclusive_overflows");
+
+   // If the user didn't define the metrics - set the default columns for the report.
+    if (IV.size() == 0) {
+     // Column[0] is inclusive time
+      IV.push_back(new ViewInstruction (VIEWINST_Display_Metric, ++Max_Column, intime_index));
+     // Column[1] is exclusive time
+      IV.push_back(new ViewInstruction (VIEWINST_Display_Metric, ++Max_Column, extime_index));
+      Max_Column = 1;
+    }
+
+   // Column[2] is % of  whatever is the first metric in the list.
+    IV.push_back(new ViewInstruction (VIEWINST_Display_Percent_Column, ++Max_Column, 0));
+
+   // The Total Time (used for % calculation) is always the total exclusive time.
+   // (Otherwise, we measure a unit of time multiple times.)
+    IV.push_back(new ViewInstruction (VIEWINST_Define_Total, extime_index));
+
+   // Get the name of the event that we were collecting.
+   // Use this for the column header in the report rather then the name of the metric.
+    std::string HV[2];
+    std::string name;
+    CV[0].getParameterValue ("event", name);
+    std::string prename;
+    for (int64_t CM_Index = 0; CM_Index < 2; CM_Index++) {
+      if (Metadata_hasName( CV[CM_Index], MV[CM_Index] )) {
+        Metadata m = Find_Metadata ( CV[CM_Index], MV[CM_Index] );
+        prename = m.getShortName();
+      } else {
+        prename = MV[CM_Index];
+      }
+      int64_t blank_at = prename.find(" ");
+      if (blank_at > 0) {
+        prename = std::string(prename, 0, blank_at);
+      }
+      HV[CM_Index] = prename + " " + name;
+    }
+
+    return Generic_View (cmd, exp, topn, tgrp, CV, MV, IV, HV);
+  }
+};
+
 
 // This is the only external entrypoint.
 // Calls to the VIEWs needs to be done through the ViewType class objects.
@@ -223,4 +326,5 @@ void Define_Basic_Views () {
   Define_New_View (new usertime_view());
   Define_New_View (new pcsamp_view());
   Define_New_View (new hwc_view());
+  Define_New_View (new hwctime_view());
 }
