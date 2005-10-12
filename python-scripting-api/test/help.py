5a39c09d185411af1cfc0d81d4a9b779da2ec241
jcarter <>
2005-10-12 15:51:20 +0000

    Temporary cli initialization call and reorder of help requests.

--- python-scripting-api/test/help.py
+++ python-scripting-api/test/help.py
@@ -1,13 +1,14 @@
 # help [<help_modifier_list>] [<keyword>]
 
 import oss
+oss.OSS_Init()
 
 my_modifer = oss.ModifierList("brief")
 my_modifer+= "examples"
 
-output = oss.oss_help()
-output = oss.oss_help(my_modifer)
 output = oss.oss_help(my_modifer,"expAttach")
+output = oss.oss_help(my_modifer)
 output = oss.oss_help("expAttach")
+output = oss.oss_help()
 
 print output
