32c0717cd20cdb1d32726da20ba7509f61ef0622
stipek <>
2006-01-12 15:27:12 +0000

    Add some io related work into the forever test case.

--- usability/phaseII/forever.c
+++ usability/phaseII/forever.c
@@ -1,5 +1,47 @@
 /* A program that never finishes */
 #include <stdio.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+void fred_read ()
+{
+  int i;
+  int d, d1, d2;
+  char buf[256];
+  char tmpname_buf[256];
+
+  pid_t pid = getpid();
+  sprintf(tmpname_buf, "/tmp/forever_fred_read%dXXXXXX", pid);
+  char *tmpname = tmpname_buf;
+
+  d = open(tmpname, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG );
+
+  if( d == -1 )
+  {
+    fprintf(stderr, "Unable to open %s\n", tmpname );
+    return;
+  }
+
+  for( i = 0; i < 300; i++ )
+  {
+    sprintf(buf, "Put this line to a tmpfile %d\n", i );
+    int bytes_written = write(d, buf, strlen(tmpname) );
+  }
+  close( d );
+  d1 = open (tmpname, O_RDONLY);
+  d2 = open (tmpname, O_RDONLY);
+  lseek (d1, 256, SEEK_SET);
+  int bytes_read = read (d2, buf, 16);
+
+  close( d1 );
+  close( d2 );
+
+  unlink( tmpname );
+} 
 
 void frank()
 {
@@ -90,8 +132,11 @@ main(int argc, char **argv)
     number = atoi(input);
     printf("Input was %d\n", number);
   }
+
+  fred_read();
+
   if( !size )
   {
-    printf("End of life for the forever program....\n");
+    printf("End of life for the program....\n");
   }
 }
