42e68a96d441d57471aa3166eeaf7f7b8552edbf
dew <>
2006-01-20 21:53:34 +0000

    Use the new generalized final formatting routine.

--- plugins/views/mpi/mpi_view.cxx
+++ plugins/views/mpi/mpi_view.cxx
@@ -766,185 +766,6 @@ static void Expand_CallStack (
   c_items = result;
 }
 
-static
-void Form_CallStack_Output (CommandObject *cmd,
-                            ThreadGroup& tgrp,
-                            std::vector<Collector>& CV,
-                            std::vector<std::string>& MV,
-                            std::vector<ViewInstruction *>& IV,
-                            int64_t num_columns,
-                            bool Gen_Total_Percent,
-                            int64_t percentofcolumn,
-                            CommandResult *TotalValue,
-                            std::vector<std::pair<CommandResult_CallStackEntry *,
-                                                  SmartPtr<std::vector<CommandResult *> > > >& items) {
-  int64_t i;
-  bool report_Column_summary = false;
-
-  std::vector<ViewInstruction *> ViewInst(num_columns);
-  int64_t num_input_temps = items[0].second->size();
-  bool input_temp_used[num_input_temps];
-  std::vector<CommandResult *> summary_temp(num_input_temps);
-  for ( i=0; i < num_input_temps; i++) summary_temp[i] = NULL;
-
- // Reformat the instructions for easier access.
-  for (i = 0; i < IV.size(); i++) {
-    ViewInstruction *vp = IV[i];
-      if ((vp->OpCode() == VIEWINST_Display_Metric) ||
-          (vp->OpCode() == VIEWINST_Display_Tmp) ||
-          (vp->OpCode() == VIEWINST_Display_Percent_Column) ||
-          (vp->OpCode() == VIEWINST_Display_Percent_Metric) ||
-          (vp->OpCode() == VIEWINST_Display_Percent_Tmp) ||
-          (vp->OpCode() == VIEWINST_Display_Average_Tmp) ||
-          (vp->OpCode() == VIEWINST_Display_StdDeviation_Tmp)) {
-        ViewInst[vp->TR()] = vp;
-      }
-      if (vp->OpCode() == VIEWINST_Display_Summary) {
-        report_Column_summary = true;
-      }
-  }
-  if (report_Column_summary) {
-    std::vector<CommandResult *> first_row = *(items.begin()->second);
-    for ( i=0; i < num_input_temps; i++) {
-      summary_temp[i] = Copy_CRType (first_row[i]);
-    }
-  }
-
-   // Format the report with the items that are in the vector.
-    std::vector<std::pair<CommandResult_CallStackEntry *,
-                          SmartPtr<std::vector<CommandResult *> > > >::iterator it;
-    for(it = items.begin(); it != items.end(); it++ ) {
-
-     // Check for asnychonous abort command
-      if (cmd->Status() == CMD_ABORTED) {
-        return;
-      }
-
-     // Local data values
-      CommandResult *percent_of = NULL;
-      CommandResult_Columns *C = new CommandResult_Columns ();
-
-      for ( i=0; i < num_input_temps; i++) {
-        input_temp_used[i] = false;
-      }
-
-     // Place values into columns.
-      for ( i=0; i < num_columns; i++) {
-        ViewInstruction *vinst = ViewInst[i];
-        int64_t CM_Index = vinst->TMP1();
-
-        CommandResult *Next_Metric_Value = NULL;
-        if (vinst->OpCode() == VIEWINST_Display_Metric) {
-          Next_Metric_Value = (*it->second)[i];
-          input_temp_used[i] = true;
-        } else if (vinst->OpCode() == VIEWINST_Display_Tmp) {
-          Next_Metric_Value = (*it->second)[vinst->TMP1()];
-          input_temp_used[CM_Index] = true;
-        } else if (vinst->OpCode() == VIEWINST_Display_Percent_Column) {
-          if (!Gen_Total_Percent) {
-           // The measured time interval is too small.
-            continue;
-          }
-          if ((i > percentofcolumn) &&
-              (percent_of != NULL)) {
-            Next_Metric_Value = Calculate_Percent (percent_of, TotalValue);
-          } else {
-            Mark_Cmd_With_Soft_Error(cmd,"Percent of column is not available for mpi view");
-            return;
-          }
-        } else if (vinst->OpCode() == VIEWINST_Display_Percent_Metric) {
-          if (Gen_Total_Percent) {
-            Mark_Cmd_With_Soft_Error(cmd,"Percent Metric value is not available for mpi view");
-            return;
-          }
-        } else if (vinst->OpCode() == VIEWINST_Display_Percent_Tmp) {
-          CommandResult *V = (*it->second)[CM_Index];
-          if (!V->isNullValue ()) {
-            Next_Metric_Value = Calculate_Percent (V, TotalValue);
-          }
-        } else if (vinst->OpCode() == VIEWINST_Display_Average_Tmp) {
-          CommandResult *V = (*it->second)[CM_Index];
-          if (!V->isNullValue ()) {
-            Next_Metric_Value = Calculate_Average (V, (*it->second)[vinst->TMP2()]);
-          }
-        } else if (vinst->OpCode() ==VIEWINST_Display_StdDeviation_Tmp) {
-          CommandResult *V1 = (*it->second)[vinst->TMP1()];
-          CommandResult *V2 = (*it->second)[vinst->TMP2()];
-          CommandResult *V3 = (*it->second)[vinst->TMP3()];
-          Next_Metric_Value = Calculate_StdDev (V1, V2, V3);
-        }
-        if (Next_Metric_Value == NULL) {
-          Next_Metric_Value = CRPTR ("");
-        }
-        C->CommandResult_Columns::Add_Column (Next_Metric_Value);
-        if (Gen_Total_Percent &&
-            (i == percentofcolumn)) {
-          percent_of = Next_Metric_Value;
-        }
-      }
-     // Add ID for row
-      C->CommandResult_Columns::Add_Column (it->first);
-      it->first = NULL;  // allow only 1 pointer to a CommandResult object
-      cmd->Result_Predefined (C);
-
-     // Accumulate Summary Information
-      if (report_Column_summary) {
-          Accumulate_PreDefined_Temps (IV, summary_temp, (*it->second));
-      }
-
-     // Reclaim space for unused temps.
-      for ( i=0; i < num_input_temps; i++) {
-        if (input_temp_used[i]) {
-         // allow only 1 pointer to a CommandResult object
-          (*it->second)[i] = NULL;
-        }
-      }
-    }
-
-    if (report_Column_summary) {
-     // Build an Ender summary for the table.
-      CommandResult_Enders *E = new CommandResult_Enders ();
-     // Add Metrics Summary
-      for ( i=0; i < num_columns; i++) {
-        ViewInstruction *sinst = ViewInst[i];
-        CommandResult *summary = NULL;
-        Assert (sinst != NULL);
-        if (sinst->OpCode() == VIEWINST_Display_Tmp) {
-          summary = Dup_CommandResult (summary_temp[sinst->TMP1()]);
-        } else if (sinst->OpCode() == VIEWINST_Display_Average_Tmp) {
-          CommandResult *V = summary_temp[sinst->TMP1()];
-          if (!V->isNullValue ()) {
-            summary = Calculate_Average (V, summary_temp[sinst->TMP2()]);
-          }
-        } else if (sinst->OpCode() == VIEWINST_Display_StdDeviation_Tmp) {
-          CommandResult *V1 = summary_temp[sinst->TMP1()];
-          CommandResult *V2 = summary_temp[sinst->TMP2()];
-          CommandResult *V3 = summary_temp[sinst->TMP3()];
-          summary = Calculate_StdDev (V1, V2, V3);
-        } else if (sinst->OpCode() == VIEWINST_Display_Percent_Tmp) {
-          CommandResult *V = summary_temp[sinst->TMP1()];
-          summary = Calculate_Percent (V, TotalValue);
-        }
-        if (summary == NULL) {
-          summary = CRPTR ("");
-        }
-
-        E->CommandResult_Enders::Add_Ender (summary);
-      }
-     // Add ID
-      E->CommandResult_Enders::Add_Ender ( CRPTR ( "Report Summary" ) );
-      cmd->Result_Predefined (E);
-    }
-
- // Release summary temporaries
-  for ( i=0; i < num_input_temps; i++) {
-    if (summary_temp[i] != NULL) {
-      delete summary_temp[i];
-    }
-  }
-
-}
-
 /**
  * Method: Reclaim_Space
  *
@@ -1252,22 +1073,32 @@ static bool Generic_mpi_View (CommandObject *cmd, ExperimentObject *exp, int64_t
           Function func = *fsi;
           Extract_Pivot_Items (cmd, exp, IV, TraceBack_Order, c_items, func, result);
           if (!result.empty()) {
-            if (MoreThanOne) cmd->Result_RawString("");  // Delimiter between items is a null string.
-            Form_CallStack_Output (cmd, tgrp, CV, MV, IV,
+            std::list<CommandResult *> view_output;
+            Construct_View_Output (cmd, tgrp, CV, MV, IV,
                                    num_columns,
                                    Gen_Total_Percent, percentofcolumn, TotalValue,
-                                   result);
+                                   result,
+                                   view_output);
+            if (!view_output.empty()) {
+              if (MoreThanOne) cmd->Result_RawString("");  // Delimiter between items is a null string.
+              cmd->Result_Predefined (view_output);
+              MoreThanOne = true;
+            }
             Reclaim_Space (0, result);
-            MoreThanOne = true;
           }
         }
       }
 
     } else {
-      Form_CallStack_Output (cmd, tgrp, CV, MV, IV,
+      std::list<CommandResult *> view_output;
+      Construct_View_Output (cmd, tgrp, CV, MV, IV,
                              num_columns,
                              Gen_Total_Percent, percentofcolumn, TotalValue,
-                             c_items);
+                             c_items,
+                             view_output);
+      if (!view_output.empty()) {
+        cmd->Result_Predefined (view_output);
+      }
     }
 
   }
