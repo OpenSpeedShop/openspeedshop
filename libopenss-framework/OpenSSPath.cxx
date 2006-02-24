a5e09fe71aa7aa3615929c9fe7c43a67549d6dac
jcarter <>
2006-02-24 22:36:49 +0000

    Fixed problem with duplicate entries in the plugin path repository.
    Actually it is better that before I started this
    because if the user had duplicates in the environment
    variables they would have been entered twice too.
    
    Jack

--- libopenss-framework/OpenSSPath.cxx
+++ libopenss-framework/OpenSSPath.cxx
@@ -22,6 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <vector>
 
 #define PATH_DELIMITER ':'
 #define RELATIVE_OPENSS_PLUGIN_PATH "/openspeedshop"
@@ -33,6 +34,101 @@
 #ifndef PLUGIN_DIR
 #define PLUGIN_DIR 0
 #endif
+
+using namespace std;
+
+/**
+ * Method: s_check_add_unique
+ * 
+ * Check if the given string is already in the given
+ * array of strings. If not, add it.
+ *
+ * If there is not enough room in the array, expand 
+ * the given array and them add it.
+ *     
+ * @param   string *input_str: string to add if unique
+ * @param   vector<string> *v_string: list of stored strings
+ *     
+ * @return  void.
+ *
+ * @todo    Error handling.
+ * @todo    Example.
+ *
+ */
+static void
+s_check_add_unique(string *input_str, vector<string> *v_string)
+{
+    vector<string>::iterator iter;
+    for (iter=v_string->begin();
+    	 iter != v_string->end(); 
+	 iter++) {
+	
+	//cout << "xxx " << *input_str << "  " << *iter << endl;
+    	if (*input_str == *iter)
+	    return;
+
+    }
+    v_string->push_back(*input_str);
+    Assert(lt_dladdsearchdir(input_str->c_str()) == 0);
+    
+    return;
+}
+
+/**
+ * Method: s_add_plugin_path
+ * 
+ * If we have duplicates of paths for looking
+ * up plugins we mess everybody up because they
+ * aren't being careful and looking for that.
+ *
+ * This routine checks to make sure only unique path
+ * strings are added. If 2 path strings are different
+ * but point to the same place it will still add them
+ * both
+ *     
+ * @param   char *path_str: possibly colon seperated paths
+ * @param   vector<string> *v_string: container of saved paths
+ * @param   bool add_dir: do we append the plugin relative dir name?
+ *     
+ * @return  void.
+ *
+ * @todo    Error handling.
+ * @todo    Example.
+ *
+ */
+static void
+s_add_plugin_path(char *path_str, vector<string> *v_string, bool add_dir) 
+{
+    	char *old_ptr = (char *)path_str;
+
+    	while(1) {
+    	    char *new_ptr = strchr(old_ptr,PATH_DELIMITER);
+	
+    	    if (!new_ptr) {
+    	    	string temp_path(old_ptr);
+		if (add_dir)
+    	    	    temp_path += RELATIVE_OPENSS_PLUGIN_PATH;
+		//std::cout << "temp_path = " << temp_path.c_str() << std::endl;
+		s_check_add_unique(&temp_path,v_string);
+    	    	break;
+	    }
+	    else {
+    	    	int size = (new_ptr) - old_ptr;
+    	    	char *p_buf = (char *)malloc(size+1+strlen(RELATIVE_OPENSS_PLUGIN_PATH));
+    
+    	    	strncpy(p_buf,old_ptr,size);
+		p_buf[size] = '\0';
+		string temp_path(p_buf);
+		if (add_dir)
+    	    	    temp_path += RELATIVE_OPENSS_PLUGIN_PATH;
+
+		//std::cout << "temp_path = " << p_buf << std::endl;
+		s_check_add_unique(&temp_path,v_string);
+
+    	    	old_ptr = new_ptr+1;
+	    }
+    	} // while
+}
 
 /**
  * Method: SetOpenssLibPath()
@@ -61,8 +157,7 @@
  *     	    	on to each entry.
  *     3)	PLUGIN_DIR
  *     
- * @param   
- * @param   
+ * @param   void
  *
  * @return  void
  *
@@ -98,7 +193,7 @@ SetOpenssLibPath()
     	}
 	else {
     	    if(getenv("LD_LIBRARY_PATH") != NULL){
-    	    	std::string ld_library_path = std::string(getenv("LD_LIBRARY_PATH"));
+    	    	string ld_library_path = string(getenv("LD_LIBRARY_PATH"));
     	     	Assert((ld_library_path_cstr = strdup(ld_library_path.c_str())) != NULL);
 	    }
 	}
@@ -124,48 +219,26 @@ SetOpenssLibPath()
     	// Start with an empty libltdl search path
     	Assert(lt_dlsetsearchpath("") == 0);
     
+    	vector<string> v_string;
     	// Add the user-specified plugin path
     	if(getenv("OPENSS_PLUGIN_PATH") != NULL) {
-    	    Assert(lt_dladdsearchdir(getenv("OPENSS_PLUGIN_PATH")) == 0);
-	    //std::cout << "OPENSS_PLUGIN_PATH = " <<  getenv("OPENSS_PLUGIN_PATH") << std::endl;
+    	    s_add_plugin_path((char *)getenv("OPENSS_PLUGIN_PATH"), 
+	    	    	      &v_string,
+			      false /* don't append /openss */ ) ;
 	}
 
-    	// Add the LD_LIBRARY_PATH relative paths.
-    	char *old_ptr = getenv("LD_LIBRARY_PATH");
+    	// Add the LD_LIBRARY_PATH re;ativ plugin path
+    	if(getenv("LD_LIBRARY_PATH") != NULL) {
+    	    s_add_plugin_path((char *)getenv("LD_LIBRARY_PATH"), 
+	    	    	      &v_string,
+			      true /* do append /openss */ ) ;
+	}
 
-    	// This is stupid code since it adds duplicate paths.
-	// Feel free to make it smarter without pulling in other
-	// parts of the tool. This needs to remain on the fully
-	// opensource side of the tool. This means, I don't think
-	// we can use Path.[ch]xx.
-    	while(1) {
-    	    char *new_ptr = strchr(old_ptr,PATH_DELIMITER);
-	
-    	    if (!new_ptr) {
-    	    	std::string temp_path(old_ptr);
-    	    	temp_path += RELATIVE_OPENSS_PLUGIN_PATH;
-		//std::cout << "temp_path = " << temp_path.c_str() << std::endl;
-    	    	Assert(lt_dladdsearchdir(temp_path.c_str()) == 0);
-    	    	break;
-	    }
-	    else {
-    	    	int size = (new_ptr) - old_ptr;
-    	    	char *p_buf = (char *)malloc(size+1+strlen(RELATIVE_OPENSS_PLUGIN_PATH));
-    
-    	    	strncpy(p_buf,old_ptr,size);
-		p_buf[size] = '\0';
-		strcat(p_buf,RELATIVE_OPENSS_PLUGIN_PATH);
-
-		//std::cout << "temp_path = " << p_buf << std::endl;
-    	    	Assert(lt_dladdsearchdir(p_buf) == 0);
-	    
-    	    	old_ptr = new_ptr+1;
-	    }
-    	} // while
-    
     	// Add our compile-time plugin directory
 	if (PLUGIN_DIR) {
-    	    Assert(lt_dladdsearchdir(PLUGIN_DIR) == 0);
+    	    s_add_plugin_path(PLUGIN_DIR,
+	    	    	      &v_string,
+			      false /* don't append /openss */) ;
 	}
 
     	//std::cout << "LD_LIBRARY_PATH = " << getenv("LD_LIBRARY_PATH") << std::endl;
