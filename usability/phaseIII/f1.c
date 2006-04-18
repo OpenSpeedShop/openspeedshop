41ddebe75bd47dd2ddeaa4c291029ba708702186
stipek <>
2006-04-18 11:50:24 +0000

    Remove unused files for phaseIII usability study.

--- usability/phaseIII/f1.c
+++
@@ -1,8 +0,0 @@
-int f1(int x, int y)
-{
-    int i, t = 0;
-
-    for(i = 0; i < 5000; i++)
-        t += x * y;
-    return t;
-}
--- usability/phaseIII/f3.c
+++
@@ -1,8 +0,0 @@
-int f3(int x, int y)
-{
-    int i, t = 0;
-
-    for(i = 0; i < 15000; i++)
-        t += x * y;
-    return t;
-}
