f0d97b97a5a8d961440204c9104cdfece49aba09
stipek <>
2006-06-15 14:48:08 +0000

    Slight modification to make fred more robust.

--- usability/phaseII/fred.c
+++ usability/phaseII/fred.c
@@ -5,16 +5,38 @@ int main(int argc, char* argv[])
 {
    extern int work(int);
    int size = 750;
+   int i = 0;
+   int segment_count  = 1;
 
-    if(argc != 2) {
-        printf("Usage: %s <size>\n", argv[0]);
-        printf("No size argument given.   Defaulting to %d.\n", size);
+    if( argc == 3 )
+    {
+      size = atoi(argv[1]);
+      segment_count = atoi(argv[2]);
+    } else if(argc != 2 || argc != 3 )
+    {
+      printf("Usage: %s <size> <segment_count>\n", argv[0]);
+      printf("No size argument given.   Defaulting to %d.\n", size);
     } else
     {
       size = atoi(argv[1]);
     }
     
+
+if( segment_count == 1 )
+{
     work(size);
+} else
+{
+  while( segment_count > 0 )
+  {
+    for(i=0; i<segment_count;i++)
+    {
+printf("call work(%d) %d\n", size, i);
+      work(size);
+    }
+    segment_count--;
+  }
+}
 
     printf("%s: succesfully completed.\n", argv[0]);
 }
