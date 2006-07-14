6bea34e978cb24b58633215568561ca3481b3db4
jeg <>
2006-07-14 13:11:04 +0000

    add wdh test case for dlopen

---
+++ test/executables/dlopen/work.c
@@ -0,0 +1,37 @@
+int f1(int x, int y)
+{
+    int i, t = 0;
+
+    for(i = 0; i < 5000; i++)
+        t += x * y;
+    return t;
+}
+
+int f2(int x, int y)
+{
+    int i, t = 0;
+
+    for(i = 0; i < 10000; i++)
+        t += x * y;
+    return t;
+}
+
+int f3(int x, int y)
+{
+    int i, t = 0;
+
+    for(i = 0; i < 15000; i++)
+        t += x * y;
+    return t;
+}
+
+int work(int size)
+{
+    int i, j, t = 0;
+
+    for(i = 0; i < size; i++)
+        for(j = 0; j < size; j++)
+            t += f3(i, j) - f2(i, j) - f1(i, j);
+    return t;
+}
+
