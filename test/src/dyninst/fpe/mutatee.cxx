299dc9d60bccf7db97b5aac47f194e489353f4e8
jeg <>
2006-08-04 21:14:25 +0000

    add the source files for a fpe dyninst only test case

---
+++ test/src/dyninst/fpe/mutatee.cxx
@@ -0,0 +1,64 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <stdint.h>
+
+void fpe_enable()
+{
+ feenableexcept(FE_ALL_EXCEPT);
+}
+
+
+/* print a message and exit */
+void fpe_handler(int sig_number, siginfo_t *info, void *data)
+{
+
+  printf("Entered mutatee.cxx: fpe_handler\n" );
+  exit(1);
+}
+
+int main(void)
+{
+    /* setup a signal handler for SIGFPE */
+    struct sigaction action, oldaction;
+
+    memset(&action, 0, sizeof(action));
+
+    action.sa_sigaction = fpe_handler;      /* which callback function */
+    sigemptyset(&action.sa_mask);           /* other signals to block */
+
+    action.sa_flags = SA_STACK | SA_ONSTACK | SA_RESTART | SA_SIGINFO;
+
+    if (sigaction(SIGFPE, &action, &oldaction)) {
+      fprintf(stderr, "error: failed to register signal handler %d (%s)\n",
+              errno, strerror(errno));
+      return 1;
+    }
+
+// to test whether we can get into the fpe handler uncomment the next line
+    fpe_enable();
+
+    sleep(60);
+
+    double aa;
+    double bb;
+    double cc;
+
+    aa = 1.0;
+    bb = time(0) - time(0);
+    cc = aa / bb;
+    printf("######### DIVIDE BY ZERO cc = aa / bb, cc=%f\n", cc);
+
+}
---
+++ test/src/dyninst/fpe/mutator.cxx
@@ -0,0 +1,44 @@
+
+#include "BPatch.h"
+#include "BPatch_function.h"
+
+BPatch bpatch;
+
+int main(int argc, char* argv[])
+{
+
+    // Display usage information when necessary
+    if(argc != 2) {
+	printf("Usage: %s <pid>\n", argv[0]);
+	exit(1);
+    }
+    
+    // Attach to the running program
+    BPatch_thread* appThread = bpatch.attachProcess(NULL, atoi(argv[1]));
+    
+    
+    // Read the program's image and get the associated image object
+    BPatch_image* appImage = appThread->getImage();
+
+    
+    // Find fpe_enable() 
+    BPatch_Vector<BPatch_function*> fpeFuncs;
+    appImage->findFunction("fpe_enable", fpeFuncs);
+    if(fpeFuncs.size() != 1) {
+	printf("Found %d occurences of fpe_enable().\n", fpeFuncs.size());
+	exit(1);
+    }
+    
+    BPatch_Vector<BPatch_snippet*> args;
+    BPatch_funcCallExpr callExp(*(fpeFuncs[0]),args );
+
+    // 
+    appThread->oneTimeCode(callExp);
+
+
+    // Continue exection of mutatee and wait for it to terminate
+    appThread->continueExecution();
+    while(!appThread->isTerminated())
+	bpatch.waitForStatusChange();
+    
+}
