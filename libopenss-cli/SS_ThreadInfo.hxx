3317359c43ce1b2cd5dc46904710d16a3e8cc554
jgalarowicz <>
2007-09-19 03:17:06 +0000

    First phase of updating copyrights in files changed by Krell where the copyrights were not updated.

--- libopenss-cli/SS_ThreadInfo.hxx
+++ libopenss-cli/SS_ThreadInfo.hxx
@@ -1,5 +1,6 @@
 /*******************************************************************************
 ** Copyright (c) 2006 Silicon Graphics, Inc. All Rights Reserved.
+** Copyright (c) 2006 Krell Institute  All Rights Reserved.
 **
 ** This library is free software; you can redistribute it and/or modify it under
 ** the terms of the GNU Lesser General Public License as published by the Free
--- libopenss-cli/SS_View_output.cxx
+++ libopenss-cli/SS_View_output.cxx
@@ -1,6 +1,7 @@
 /*******************************************************************************
       std::vector<CommandResult *>& Total_Values) {
 ** Copyright (c) 2005 Silicon Graphics, Inc. All Rights Reserved.
+** Copyright (c) 2006 Krell Institute  All Rights Reserved.
 **
 ** This library is free software; you can redistribute it and/or modify it under
 ** the terms of the GNU Lesser General Public License as published by the Free
