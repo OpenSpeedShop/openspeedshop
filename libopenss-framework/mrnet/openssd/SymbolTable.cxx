6461a768353d873c4ca2be550e5ac29c43f7256e
whachfeld <>
2007-12-13 04:49:26 +0000

    Add recovered SymbolTable class files.

---
+++ libopenss-framework/mrnet/openssd/SymbolTable.cxx
@@ -0,0 +1,331 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2007 William Hachfeld. All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+/** @file
+ *
+ * Definition of the SymbolTable class.
+ *
+ */
+
+#include "SymbolTable.hxx"
+
+#include <BPatch_function.h>
+#include <deque>
+
+using namespace OpenSpeedShop::Framework;
+
+
+
+/**
+ * Default constructor.
+ *
+ * Constructs an empty symbol table.
+ */
+SymbolTable::SymbolTable() :
+    dm_functions(),
+    dm_statements()
+{
+}
+
+
+
+/**
+ * Constructor from a BPatch_module object.
+ *
+ * Constructs a symbol table containing the symbols of the specified module.
+ *
+ * @param module    Module from which to construct the symbol table.
+ */
+SymbolTable::SymbolTable(/* const */ BPatch_module& module) :
+    dm_functions(),
+    dm_statements()
+{
+    addModule(module);
+}
+
+
+
+/**
+ * Add a module.
+ *
+ * Add the symbols of the specified module to this symbol table.
+ *
+ * @param module    Module to be added.
+ */
+void SymbolTable::addModule(/* const */ BPatch_module& module)
+{
+    // Get the list of functions in this module
+    BPatch_Vector<BPatch_function*>* functions = module.getProcedures(true);
+    Assert(functions != NULL);
+    
+    // Iterate over each function in this module
+    for(int i = 0; i < functions->size(); ++i) {
+	Assert((*functions)[i] != NULL);
+
+	// Get all the mangled names of this function
+	BPatch_Vector<const char*> names;
+	(*functions)[i]->getMangledNames(names);
+
+	// TODO: how do we get the possibly discontiguous range of a function?
+
+	// Get the address range of the function
+	Address begin(
+	    reinterpret_cast<uint64_t>((*functions)[i]->getBaseAddr())
+	    );
+	Address end = begin + (*functions)[i]->getSize();
+	AddressRange range(begin, end);
+
+	// Iterate over each name of this function
+	for(int j = 0; j < names.size(); ++j) {
+
+	    // Add this function to the table (or find the existing entry)
+	    std::map<std::string, std::vector<AddressRange> >::iterator k =
+		dm_functions.insert(
+		    std::make_pair(names[j], std::vector<AddressRange>())
+		    ).first;
+	    
+	    // Add this address range to the found/added function
+	    k->second.push_back(range);
+
+	}
+    }
+
+    // Get the list of statements in this module
+    std::vector<struct BPatch_module::Statement> statements =
+	module.getStatements();
+    
+    // Iterate over each statement in this module
+    for(int i = 0; i < statements.size(); ++i) {
+
+	// Construct a StatementEntry for this statement
+	StatementEntry entry(FileName(statements[i].path),
+			     statements[i].line, statements[i].column);
+
+	// Add this statement to the table (or find the existing entry)
+	std::map<StatementEntry, std::vector<AddressRange> >::iterator j =
+	    dm_statements.insert(
+	        std::make_pair(entry, std::vector<AddressRange>())
+		).first;
+
+	// Add this address range to the found/added statement
+	j->second.push_back(AddressRange(Address(statements[i].begin),
+					 Address(statements[i].end)));
+    }
+}
+
+
+
+/**
+ * Conversion to OpenSS_Protocol_SymbolTable.
+ *
+ * Convert this symbol table to an OpenSS_Protocol_SymbolTable object.
+ *
+ * @note    The "experiments" and "linked_object" fields of the returned object
+ *          are not initialized to any value. It is the responsibility of the
+ *          caller to fill in these fields.
+ *
+ * @return    An OpenSS_Protocol_SymbolTable containing the contents of this
+ *            symbol table.
+ */
+SymbolTable::operator OpenSS_Protocol_SymbolTable() const
+{
+    OpenSS_Protocol_SymbolTable object;
+
+    // Allocate an array (if necessary) to hold the functions
+    object.functions.functions_len = dm_functions.size();
+    if(!dm_functions.empty()) {
+	object.functions.functions_val = 
+	    new OpenSS_Protocol_FunctionEntry[dm_functions.size()];
+	
+	// Iterate over all the functions in this symbol table
+	int idx = 0;
+	for(std::map<std::string, std::vector<AddressRange> >::const_iterator
+		i = dm_functions.begin(); i != dm_functions.end(); ++i, ++idx) {
+	    OpenSS_Protocol_FunctionEntry& entry = 
+		object.functions.functions_val[idx];
+	    
+	    // Convert the function's name
+	    if(!i->first.empty()) {
+		entry.name = new char[i->first.size()];
+		strcpy(entry.name, i->first.c_str());
+	    }
+	    else
+		entry.name = NULL;
+	    
+	    // Partition this function's address ranges into bitmaps
+	    std::vector<AddressBitmap> bitmaps =
+		partitionAddressRanges(i->second);
+
+	    // TODO: convert the function's bitmaps
+	    
+	}
+
+    }
+
+    // Allocate an array (if necessary) to hold the statements
+    object.statements.statements_len = dm_statements.size();
+    if(!dm_statements.empty()) {
+	object.statements.statements_val =
+	    new OpenSS_Protocol_StatementEntry[dm_statements.size()];
+
+	// Iterate over all the statements in this symbol table
+	int idx = 0;
+	for(std::map<StatementEntry, std::vector<AddressRange> >::const_iterator
+		i = dm_statements.begin();
+	    i != dm_statements.end();
+	    ++i, ++idx) {
+	    OpenSS_Protocol_StatementEntry& entry =
+		object.statements.statements_val[idx];
+
+	    // Convert the statement's path, line, and column
+	    entry.path = i->first.dm_path;
+	    entry.line = i->first.dm_line;
+	    entry.column = i->first.dm_column;
+
+	    // Partition this statement's address ranges into bitmaps
+	    std::vector<AddressBitmap> bitmaps =
+		partitionAddressRanges(i->second);
+
+	    // TODO: convert the statement's bitmaps
+
+	}
+    }
+
+    // Return the conversion to the caller
+    return object;
+}
+
+
+
+/**
+ * Partition address ranges.
+ *
+ * Partitions address ranges into address bitmaps. The addresses for a function
+ * or statement are stored as pairings of an address range and a bitmap - one
+ * bit per address in the range - that describes which addresses within the
+ * range are associated with the function or statement. In the common case where
+ * the addresses exhibit a high degree of spatial locality, storing a single
+ * address range and bitmap is very effective. But there are cases, such as
+ * inlined functions, where the degree of spatial locality is minimal. Under
+ * such circumstances, a single bitmap can grow very large and it is more space
+ * efficient to use multiple bitmaps that individually exhibit spatial locality.
+ * This function iteratively subdivides all the addresses until each bitmap
+ * exhibits a "sufficient" amount of spatial locality.
+ *
+ * @note    The criteria for subidiving an address set is as follows. The
+ *          widest gap (spacing) between two adjacent addresses is found. If
+ *          the number of bits required to encode this gap in the bitmap is
+ *          greater than the number of bits required to create an AddressRange
+ *          object, then the set is partitioned at this, widest, gap.
+ *
+ * @param ranges    Address ranges to be partitioned.
+ * @return          Address bitmaps representing these address ranges.
+ */
+std::vector<AddressBitmap>
+SymbolTable::partitionAddressRanges(const std::vector<AddressRange>& ranges)
+{
+    std::vector<AddressBitmap> bitmaps;
+
+    // Set the partitioning criteria
+    static const Address::difference_type PartitioningCriteria =
+	8 * (sizeof(uint32_t) + 2 * sizeof(uint64_t));
+
+    // Construct the set of unique addresses in these address ranges
+    std::set<Address> addresses;
+    for(std::vector<AddressRange>::const_iterator
+	    i = ranges.begin(); i != ranges.end(); ++i)
+	for(Address j = i->getBegin(); j != i->getEnd(); ++j)
+	    addresses.insert(j);
+    
+    // Initialize a queue with this initial, input, set of addresses
+    std::deque<std::set<Address> > queue(1, addresses);
+
+    // Iterate until the queue is empty
+    while(!queue.empty()) {
+        std::set<Address> i = queue.front();
+        queue.pop_front();
+
+	// Handle special case for empty sets (ignore them)
+        if(i.size() == 0)
+            continue;
+
+	// Handle special case for single-element sets
+        if(i.size() == 1) {
+
+	    // Create and populate an address bitmap for this address set
+	    AddressBitmap bitmap(AddressRange(*(i.begin()), *(i.rbegin()) + 1));
+	    for(std::set<Address>::const_iterator
+                    j = i.begin(); j != i.end(); ++j)
+                bitmap.setValue(*j, true);
+
+	    // Add this bitmap to the results
+	    bitmaps.push_back(bitmap);
+	    
+            continue;
+        }
+        
+	// Pair specifying the widest gap in this address set
+        std::pair<std::set<Address>::const_iterator, Address::difference_type>
+            widest_gap = std::make_pair(i.begin(), 0);
+	
+        // Iterate over each adjacent pair of addresses in this set
+        for(std::set<Address>::const_iterator
+		prev = i.begin(), current = ++i.begin();
+            current != i.end();
+            ++prev, ++current) {
+
+            // Calculate the gap between this adjacent address pair
+            Address::difference_type gap =
+                AddressRange(*prev, *current).getWidth() - 1;
+
+	    // Is this gap the widest so far?
+            if(gap > widest_gap.second) {
+		widest_gap.first = current;
+                widest_gap.second = gap;
+            }
+            
+        }
+
+	// Is the widest gap greater than our partitioning criteria?
+        if(widest_gap.second > PartitioningCriteria) {
+
+	    // Partition the set at this gap
+	    queue.push_back(std::set<Address>(i.begin(), widest_gap.first));
+            queue.push_back(std::set<Address>(widest_gap.first, i.end()));
+
+	}
+        
+        // Otherwise keep this set unpartitioned
+        else {
+
+	    // Create and populate an address bitmap for this address set
+	    AddressBitmap bitmap(AddressRange(*(i.begin()), *(i.rbegin()) + 1));
+            for(std::set<Address>::const_iterator
+                    j = i.begin(); j != i.end(); ++j)
+                bitmap.setValue(*j, true);
+
+	    // Add this bitmap to the results
+	    bitmaps.push_back(bitmap);
+
+	}
+
+    }
+
+    // Return the bitmaps to the caller
+    return bitmaps;
+}
---
+++ libopenss-framework/mrnet/openssd/SymbolTable.hxx
@@ -0,0 +1,130 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2007 William Hachfeld. All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License.
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+////////////////////////////////////////////////////////////////////////////////
+
+/** @file
+ *
+ * Declaration of the SymbolTable class.
+ *
+ */
+
+#ifndef _OpenSpeedShop_Framework_SymbolTable_
+#define _OpenSpeedShop_Framework_SymbolTable_
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "AddressBitmap.hxx"
+#include "AddressRange.hxx"
+#include "FileName.hxx"
+#include "Protocol.h"
+
+#include <BPatch.h>
+#include <map>
+#include <string>
+#include <vector>
+
+
+
+namespace OpenSpeedShop { namespace Framework {
+    
+    /**
+     * Symbol table.
+     *
+     * Table containing the symbols for a single linked object. Encapsulates
+     * the retrieval of the symbols from Dyninst and the conversion of those
+     * symbols into the format of the frontend/backend communication protocol.
+     *
+     * @note    This class has intentionally been given an identical name to
+     *          the similar class in the framework library itself. In the near
+     *          future, the experiment database functionality may be split into
+     *          a separate library and utilized by the daemon. If that happens,
+     *          this class will be removed in favor of the existing framework
+     *          class.
+     *    
+     * @ingroup Implementation
+     */   
+    class SymbolTable
+    {
+	
+    public:
+	
+	SymbolTable();
+	SymbolTable(/* const */ BPatch_module&);
+	
+	void addModule(/* const */ BPatch_module&);
+	
+	operator OpenSS_Protocol_SymbolTable() const;
+	
+    private:
+	
+	static std::vector<AddressBitmap>
+	partitionAddressRanges(const std::vector<AddressRange>&);
+	
+	/** Functions in this symbol table. */
+	std::map<std::string, std::vector<AddressRange> > dm_functions;
+	
+	/**
+	 * Statement entry.
+	 * 
+	 * Structure for a source statement's entry in the symbol table's
+	 * internal tables. Contains the full path name, line number, column
+	 * number, and address bitmaps of the source statement.
+	 */
+	struct StatementEntry
+	{
+
+	    FileName dm_path;  /**< Name of this statement's source file. */
+	    int dm_line;       /**< Line number of this statement. */
+	    int dm_column;     /**< Column number of this statement. */
+	    
+	    /** Constructor from fields. */
+	    StatementEntry(const FileName& path,
+			   const int& line, const int& column) :
+		dm_path(path),
+		dm_line(line),
+		dm_column(column)
+	    {
+	    }
+	    
+	    /** Operator "<" defined for two StatementEntry objects. */
+	    bool operator<(const StatementEntry& other) const
+	    {
+		if(dm_path < other.dm_path)
+		    return true;
+		if(dm_path > other.dm_path)
+		    return false;
+		if(dm_line < other.dm_line)
+		    return true;
+		if(dm_line > other.dm_line)
+		    return false;
+		return dm_column < other.dm_column;
+	    }
+
+	};
+	
+	/** Statements in this symbol table. */
+	std::map<StatementEntry, std::vector<AddressRange> > dm_statements;
+
+    };
+
+} }
+
+
+
+#endif
