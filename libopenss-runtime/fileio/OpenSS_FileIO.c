14a0725a3a316873f806ae416ff7ab0328514ab6
dmaghrak <>
2008-01-08 18:51:27 +0000

    Create file io runtime support for offline and mrnet.  This code was previously part of the offline collectors.  The openss_send.c code was moved from libopenss-runtime/offline.  The OpenSS_FileIO.c and OpenSS_FileIO.h files contained code refactored out of the collector runtime offline support files.

---
+++ libopenss-runtime/fileio/OpenSS_FileIO.c
@@ -0,0 +1,85 @@
+/*******************************************************************************
+** Copyright (c) The Krell Institute (2008). All Rights Reserved.
+**
+** This library is free software; you can redistribute it and/or modify it under
+** the terms of the GNU Lesser General Public License as published by the Free
+** Software Foundation; either version 2.1 of the License, or (at your option)
+** any later version.
+**
+** This library is distributed in the hope that it will be useful, but WITHOUT
+** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+** FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+** details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this library; if not, write to the Free Software Foundation, Inc.,
+** 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*******************************************************************************/
+
+/** @file
+ *
+ * Declaration and definition of the OpenSS_FileIO funstions for
+ * the offline and mrnet collector runtimes.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "RuntimeAPI.h"
+#include "OpenSS_FileIO.h"
+
+pid_t create_OpenSS_exepath () {
+    pid_t pid = getpid();
+    char tmpname[PATH_MAX];
+
+    /* find our exe path (read symlink for /proc/pid/exe) */
+    OpenSS_Path_From_Pid(tmpname);
+    OpenSS_exepath = strdup(tmpname);
+    return pid;
+}
+
+void OpenSS_CreateFilePrefix (char *collectorname) {
+    if (collectorname == NULL) {
+	fprintf(stderr,"Error: invalid prefix name\n");
+	abort();
+    }
+
+    pid_t pid = create_OpenSS_exepath();
+    char tmpname[PATH_MAX], dirname[PATH_MAX],*bname;
+
+    bname = basename(OpenSS_exepath);
+    sprintf(dirname,"/tmp/%s-%s-%d",collectorname,bname,pid);
+
+    /* create a subdirectory in /tmp (TODO: Allow overriding /tmp)
+     * to hold resulting raw output files. */
+    int rval = mkdir(dirname, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    sprintf(tmpname,"%s/%s-%d",dirname,bname,pid);
+    OpenSS_rawprefix = strdup(tmpname);
+}
+
+void OpenSS_CreateOutfile (char *suffix) {
+    uint64_t (*realfunc)() = dlsym (RTLD_NEXT, "pthread_self");
+    if ( (*realfunc) != NULL) {
+	OpenSS_rawtid = (*realfunc)();
+    }
+
+    char tmpname[PATH_MAX];
+
+    /* create pathname to write raw data for specific thread */
+    if (OpenSS_rawtid > 0) {
+        sprintf(tmpname,"%s-%lu.%s",OpenSS_rawprefix,OpenSS_rawtid,suffix);
+    }
+    else {
+        sprintf(tmpname,"%s.%s",OpenSS_rawprefix,suffix);
+    }
+
+    /* create our openss-raw output filename */
+    OpenSS_outfile = strdup(tmpname);
+#ifndef NDEBUG
+    if (getenv("OPENSS_DEBUG_FILEIO") != NULL) {
+	fprintf(stderr,"OpenSS_CreateOutfile: OpenSS_outfile = %s\n",OpenSS_outfile);
+    }
+#endif
+}
---
+++ libopenss-runtime/fileio/openss_send.c
@@ -0,0 +1,68 @@
+/*******************************************************************************
+** Copyright (c) 2007 Krell Institute. All Rights Reserved.
+**
+** This library is free software; you can redistribute it and/or modify it under
+** the terms of the GNU Lesser General Public License as published by the Free
+** Software Foundation; either version 2.1 of the License, or (at your option)
+** any later version.
+**
+** This library is distributed in the hope that it will be useful, but WITHOUT
+** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+** FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+** details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this library; if not, write to the Free Software Foundation, Inc.,
+** 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*******************************************************************************/
+
+/** @file
+ *
+ * Definition of the openss_send() function for offline and mrnet collectors.
+ *
+ */
+
+#include <rpc/rpc.h>
+
+#include <stdio.h>
+
+extern char *OpenSS_outfile;
+
+/**
+ * Write blob data.
+ *
+ * Writes blob data to the file named by OpenSS_outfile.
+ * Any header generation and data encoding is
+ * performed by OpenSS_Send() or OpenSS_SendRecord().
+ * Here the data is treated purely as a buffer of
+ * bytes to be written.
+ *
+ * @param size    Size of the data to be sent (in bytes).
+ * @param data    Pointer to the data to be written.
+ * @return        Integer "1" if succeeded or "0" if failed.
+ */
+int
+openss_send(const unsigned size, const void* data)
+{
+     /* TODO: need to add error checking to the file writing code */
+
+#ifndef NDEBUG
+    if ( (getenv("OPENSS_DEBUG_FILEIO") != NULL)) {
+	fprintf(stderr,"OPENSS_DEBUG_FILEIO openss_send: size=%d, data=%p, file=%s\n",
+           size, data,OpenSS_outfile);
+    }
+#endif
+
+    fflush(stderr);
+
+    FILE *outfile = fopen(OpenSS_outfile, "a");
+
+    /* write the size of the payload */
+    fprintf(outfile,"%d",size);
+
+    /* write the actual payload */
+    size_t res = fwrite(data, sizeof(char), size, outfile);
+
+    fclose(outfile);
+    return 1;
+}
