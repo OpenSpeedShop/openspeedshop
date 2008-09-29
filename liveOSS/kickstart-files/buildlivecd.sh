ee8ee1f18e686a3f9e782ec88c61bda8efa8f3b4
samuelgutierrez <>
2008-09-29 16:42:02 +0000

    Simple updates...

--- liveOSS/kickstart-files/buildlivecd.sh
+++ liveOSS/kickstart-files/buildlivecd.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-livecd-creator --config=livecd-fedora-8-openss.ks --fslabel=OpenSpeedShopLive
+livecd_label=OpenSpeedShop-Live
+livecd_kickstart_file=livecd-fedora-8-openss.ks
+
+#You must run livecd-creator as root...
+if [ `whoami` != "root" ]; then
+    echo "remember: you must be root to run livecd-creator..."
+fi
+
+su -c "livecd-creator --config=$livecd_kickstart_file \
+--fslabel=$livecd_label" root
