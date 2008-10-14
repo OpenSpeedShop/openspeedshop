9060d60e539fb13eaa04cac3285c56d13be799ad
dmaghrak <>
2008-10-14 00:15:27 +0000

    Fix extraneous spaces in all_trace_mpi...

--- plugins/collectors/mpit/MPITTraceableFunctions.h
+++ plugins/collectors/mpit/MPITTraceableFunctions.h
@@ -87,19 +87,19 @@
     };
 
 #if defined (OPENSS_OFFLINE)
-    char *all_traceable_mpi =
-	"MPI_Allgather:MPI_Allgatherv:MPI_Allreduce:MPI_Alltoall:\
-	MPI_Alltoallv:MPI_Barrier:MPI_Bcast:MPI_Bsend:MPI_Bsend_init:\
-	MPI_Cancel:MPI_Cart_create:MPI_Cart_sub:MPI_Comm_create:\
-	MPI_Comm_dup:MPI_Comm_free:MPI_Comm_split:MPI_Finalize:\
-	MPI_Gather:MPI_Gatherv:MPI_Get_count:MPI_Graph_create:\
-	MPI_Ibsend:MPI_Init:MPI_Intercomm_create:MPI_Intercomm_merge:\
-	MPI_Iprobe:MPI_Irecv:MPI_Irsend:MPI_Isend:MPI_Issend:\
-	MPI_Pack:MPI_Probe:MPI_Recv:MPI_Recv_init:MPI_Reduce:\
-	MPI_Reduce_scatter:MPI_Request_free:MPI_Rsend:MPI_Rsend_init:\
-	MPI_Scan:MPI_Scatter:MPI_Scatterv:MPI_Send:MPI_Sendrecv:\
-	MPI_Sendrecv_replace:MPI_Ssend:MPI_Ssend_init:MPI_Start:\
-	MPI_Startall:MPI_Test:MPI_Testall:MPI_Testany:MPI_Testsome:\
-	MPI_Unpack:MPI_Wait:MPI_Waitall:MPI_Waitany:MPI_Waitsome";
+char *all_traceable_mpi =
+"MPI_Allgather:MPI_Allgatherv:MPI_Allreduce:MPI_Alltoall:\
+MPI_Alltoallv:MPI_Barrier:MPI_Bcast:MPI_Bsend:MPI_Bsend_init:\
+MPI_Cancel:MPI_Cart_create:MPI_Cart_sub:MPI_Comm_create:\
+MPI_Comm_dup:MPI_Comm_free:MPI_Comm_split:MPI_Finalize:\
+MPI_Gather:MPI_Gatherv:MPI_Get_count:MPI_Graph_create:\
+MPI_Ibsend:MPI_Init:MPI_Intercomm_create:MPI_Intercomm_merge:\
+MPI_Iprobe:MPI_Irecv:MPI_Irsend:MPI_Isend:MPI_Issend:\
+MPI_Pack:MPI_Probe:MPI_Recv:MPI_Recv_init:MPI_Reduce:\
+MPI_Reduce_scatter:MPI_Request_free:MPI_Rsend:MPI_Rsend_init:\
+MPI_Scan:MPI_Scatter:MPI_Scatterv:MPI_Send:MPI_Sendrecv:\
+MPI_Sendrecv_replace:MPI_Ssend:MPI_Ssend_init:MPI_Start:\
+MPI_Startall:MPI_Test:MPI_Testall:MPI_Testany:MPI_Testsome:\
+MPI_Unpack:MPI_Wait:MPI_Waitall:MPI_Waitany:MPI_Waitsome";
 	
 #endif
