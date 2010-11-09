59bb9640297b7914f64c5e184eaad37556948b75
jgalarowicz <>
2010-11-09 04:55:15 +0000

    Update the copyright dates.

--- libopenss-cli/SS_Settings.hxx
+++ libopenss-cli/SS_Settings.hxx
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2005 Silicon Graphics, Inc. All Rights Reserved.
-// Copyright (c) 2006 Krell Institute  All Rights Reserved.
+// Copyright (c) 2006-2010 Krell Institute  All Rights Reserved.
 
 //
 // This library is free software; you can redistribute it and/or modify it under
