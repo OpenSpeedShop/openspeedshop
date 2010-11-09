59b21ddf03a83956535a87ef81c026b02654a60d
jgalarowicz <>
2010-11-09 04:59:39 +0000

    Update the copyrights for cxx files.

--- libopenss-cli/SS_Configure.cxx
+++ libopenss-cli/SS_Configure.cxx
@@ -1,6 +1,6 @@
 /******************************************************************************e
 ** Copyright (c) 2005 Silicon Graphics, Inc. All Rights Reserved.
-** Copyright (c) 2007, 2008 Krell Institute  All Rights Reserved.
+** Copyright (c) 2006-2010 Krell Institute  All Rights Reserved.
 **
 ** This library is free software; you can redistribute it and/or modify it under
 ** the terms of the GNU Lesser General Public License as published by the Free
