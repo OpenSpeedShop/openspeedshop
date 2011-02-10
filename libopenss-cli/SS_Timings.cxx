44a1f97bc7b19e03a8db21f4cf9f98bb8545fdc6
jgalarowicz <>
2011-02-10 22:32:47 +0000

    Update the copyright date.

--- libopenss-cli/SS_Timings.cxx
+++ libopenss-cli/SS_Timings.cxx
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2006-2010 Krell Institute  All Rights Reserved.
+// Copyright (c) 2006-2011 Krell Institute  All Rights Reserved.
 //
 // This library is free software; you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
