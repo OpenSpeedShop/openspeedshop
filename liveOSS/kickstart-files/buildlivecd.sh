337bb19cac1d1ba5915c7ef5da261487ab70b581
jgalarowicz <>
2011-10-08 03:24:15 +0000

    Update liveCD to use fc15 kickstart file

--- liveOSS/kickstart-files/buildlivecd.sh
+++ liveOSS/kickstart-files/buildlivecd.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 livecd_label=OpenSpeedShop-Live
-livecd_kickstart_file=livecd-fedora-13-openss.ks
+livecd_kickstart_file=livecd-fedora-15-openss.ks
 
 #You must run livecd-creator as root...
 if [ `whoami` != "root" ]; then
     echo "remember: you must be root to run livecd-creator..."
 fi
 
-su -c "livecd-creator --config=$livecd_kickstart_file \
---fslabel=$livecd_label" root
+# su -c "livecd-creator -d -v --config=$livecd_kickstart_file \
+su -c "livecd-creator --config=$livecd_kickstart_file --fslabel=$livecd_label" root
