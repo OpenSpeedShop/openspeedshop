fa2152cc22382b5f1a47201eb7d9288269184fce
jgalarowicz <>
2012-09-21 03:21:32 +0000

    Update the copyrights on these files.

--- libopenss-cli/SS_Parse_Result.hxx
+++ libopenss-cli/SS_Parse_Result.hxx
@@ -1,6 +1,6 @@
 /*******************************************************************************
 ** Copyright (c) 2005 Silicon Graphics, Inc. All Rights Reserved.
-** Copyright (c) 2007-2010 Krell Institute  All Rights Reserved.
+** Copyright (c) 2007-2012 Krell Institute  All Rights Reserved.
 **
 ** This library is free software; you can redistribute it and/or modify it under
 ** the terms of the GNU Lesser General Public License as published by the Free
