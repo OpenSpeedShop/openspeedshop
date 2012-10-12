6ceb3b2a1cb504d64622c73e907d47b23141617a
dmaghrak <>
2012-10-12 19:28:37 +0000

    Add dl_iterate_phdr based code contributed by Matt LeGendre to offline runtime support.  Code is currently enabled by default and can be disabled by not defining OPENSS_USE_DL_ITERATE in the build. This should support all platforms including the BGP/Q.  The dl_iterate_phdr based solution is required for BGP/Q so undefining OPENSS_USE_DL_ITERATE would break that platform. So some config work may be needed if we need to fallback to the proc maps code previously used for all platforms.  Once the code to determine static executables is added, then for BGP/Q we can determine at runtime whether to use the static address space or find the dsos in use (or dlopened/dlclosed).

--- libopenss-runtime/offline/OpenSS_GetDLInfo.c
+++ libopenss-runtime/offline/OpenSS_GetDLInfo.c
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * ** Copyright (c) 2006-2012 The Krell Institue. All Rights Reserved.
+ * ** Copyright (c) 2012 Matt LeGendre. All Rights Reserved.
  * **
  * ** This library is free software; you can redistribute it and/or modify it under
  * ** the terms of the GNU Lesser General Public License as published by the Free
@@ -24,6 +25,7 @@
 #include <inttypes.h>
 #endif
 
+#include "monlibs.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -35,8 +37,13 @@
 #include <errno.h>
 #include <dlfcn.h>
 #include <pthread.h>
+#include <link.h>
+
+#define OPENSS_USE_DL_ITERATE 1
 
 extern void offline_record_dso(const char* dsoname, uint64_t begin, uint64_t end, uint8_t is_dlopen);
+extern const char* OpenSS_GetExecutablePath();
+
 #if defined(TARGET_OS_BGP)
 #include <link.h>
 extern etext;
@@ -47,12 +54,163 @@ extern etext;
 extern edata;
 #endif
 
+const char *monlibs_vdso_name = "[vdso]";
+const char *monlibs_exe_name = "[exe]";
+static struct dl_phdr_info *cur_list = NULL;
+static unsigned int cur_list_size = 0;
+static struct dl_phdr_info *new_list = NULL;
+static unsigned int new_list_size = 0;
+#define LIST_INITIAL_SIZE 32
+static unsigned int lists_max_size = 0;
+
+static int dl_cb(struct dl_phdr_info *info, size_t size, void *data)
+{
+   if (new_list_size >= lists_max_size) {
+      if (lists_max_size)
+         lists_max_size *= 2;
+      else
+         lists_max_size = LIST_INITIAL_SIZE;
+      cur_list = (struct dl_phdr_info *) realloc(cur_list, sizeof(struct dl_phdr_info) * lists_max_size);
+      new_list = (struct dl_phdr_info *) realloc(new_list, sizeof(struct dl_phdr_info) * lists_max_size);
+   }
+
+   memcpy(new_list + new_list_size, info, sizeof(struct dl_phdr_info));
+   new_list_size++;
+   return 0;
+}
+
+static int dl_phdr_cmp(const void *a, const void *b)
+{
+   struct dl_phdr_info *first = (struct dl_phdr_info *) a;
+   struct dl_phdr_info *second = (struct dl_phdr_info *) b;
+   return first->dlpi_phdr > second->dlpi_phdr;
+}
+
+static void report(struct dl_phdr_info *pinfo, int is_load, library_cb cb)
+{
+   mem_region mr[MAX_LOAD_SEGMENTS];
+   unsigned int i, j = 0;
+   ElfW(Addr) base_addr = pinfo->dlpi_addr;
+   const char *name = NULL;
+
+   if (is_load)
+   {
+      for (i = 0; i < pinfo->dlpi_phnum; i++) {
+
+         const ElfW(Phdr) *phdr = pinfo->dlpi_phdr + i;
+         if (phdr->p_type != PT_LOAD)
+            continue;
+
+         mr[j].mem_addr = base_addr + phdr->p_vaddr;
+         mr[j].mem_size = phdr->p_memsz;
+         mr[j].file_offset = phdr->p_offset;
+         mr[j].file_size = phdr->p_filesz;
+         mr[j].premissions = (phdr->p_flags & 7);
+         j++;
+      }
+
+      if (pinfo->dlpi_name[0] == '\0') {
+         /* Both the executable and vdso names are given as empty strings.  Fill in with constant strings */
+         if (j == 1 && mr[0].mem_size <= 0x1000) {
+            /* One segment less than a single page in size implies VDSO on linux */
+            name = monlibs_vdso_name;
+         }
+         else {
+            name = OpenSS_GetExecutablePath();
+         }
+      }
+      else {
+         name = strdup(pinfo->dlpi_name);
+      }
+
+      pinfo->dlpi_name = name;
+   }
+   else {
+      name = pinfo->dlpi_name;
+   }
+
+   cb(base_addr, name, mr, j, is_load);
+
+   if (!is_load) {
+      if (name != monlibs_vdso_name && name != monlibs_exe_name) {
+         free((void *) name);
+      }
+   }
+}
+
+void monlibs_getLibraries(library_cb cb)
+{
+   unsigned c = 0, n = 0;
+   struct dl_phdr_info *temp;
+
+   /* Fill in the new list with the current libraries. */
+   new_list_size = 0;
+   dl_iterate_phdr(dl_cb, NULL);
+
+   /* Sort the new list for fast comparison */
+   qsort(new_list, new_list_size, sizeof(struct dl_phdr_info), dl_phdr_cmp);
+
+   /* Look for differences between orig and new list */
+   for (;;) {
+      if (c == cur_list_size && n == new_list_size) {
+         /* End of both lists */
+         break;
+      }
+      else if (c == cur_list_size) {
+         /* End of cur list, but still have new list.   New object. */
+         report(new_list + n++, 1, cb);
+      }
+      else if (n == new_list_size) {
+         /* End of new list, but still have cur list.  Deleted object */
+         report(cur_list + c++, 0, cb);
+      }
+      else if (cur_list[c].dlpi_phdr == new_list[n].dlpi_phdr) {
+         /* Same element in old and new list.  No change here. */
+         new_list[n++].dlpi_name = cur_list[c++].dlpi_name;
+      }
+      else if (cur_list[c].dlpi_phdr < new_list[n].dlpi_phdr) {
+         /* There's an element in the cur list that we didn't see in the new list.  Deleted object */
+         report(cur_list + c++, 0, cb);
+      }
+      else if (cur_list[c].dlpi_phdr > new_list[n].dlpi_phdr) {
+         /* There's an element in the new list that we didn't see in the cur list.  New object */
+         report(new_list + n++, 1, cb);
+      }
+   }
+
+   /* Swap the new list with the cur list.  Empty (but leave allocated) the new list */
+   temp = cur_list;
+   cur_list = new_list;
+   new_list = temp;
+   cur_list_size = new_list_size;
+   new_list_size = 0;
+}
+
+static void lc(ElfW(Addr) base_address, const char *name, mem_region *regions, unsigned int num_regions, int is_load)
+
+{
+#ifndef NDEBUG
+    if ( (getenv("OPENSS_DEBUG_COLLECTOR") != NULL)) {
+	fprintf(stderr, "OPENSS_GetDLInfo: %s %s at [%#lx, %#lx]\n",
+		is_load ? "Loaded" : "Unloaded", name,
+		regions[0].mem_addr,
+		regions[0].mem_addr + regions[0].mem_size);
+   }
+#endif
+   
+    offline_record_dso(name, regions[0].mem_addr, regions[0].mem_addr + regions[0].mem_size, is_load);
+}
+
 int OpenSS_GetDLInfo(pid_t pid, char *path)
 {
 #if defined(TARGET_OS_BGP)
     offline_record_dso(OpenSS_GetExecutablePath(),(uint64_t)0x01000000,(uint64_t)&etext,0);
 #elif defined(TARGET_OS_BGQ)
     offline_record_dso(OpenSS_GetExecutablePath(),(uint64_t)0x01000000,(uint64_t)&etext,0);
+#else
+
+#if defined(OPENSS_USE_DL_ITERATE)
+    monlibs_getLibraries(lc);
 #else
     char mapfile_name[PATH_MAX];
     FILE *mapfile;
@@ -126,13 +284,14 @@ int OpenSS_GetDLInfo(pid_t pid, char *path)
 	else if (perm[2] == 'x' && path == NULL) {
 #ifndef NDEBUG
 	    if ( (getenv("OPENSS_DEBUG_COLLECTOR") != NULL)) {
-		fprintf(stderr,"OpenSS_GetDLInfo LD RECORD %s\n", mappedpath);
+		fprintf(stderr,"OpenSS_GetDLInfo LD RECORD %s [%08lx, %08lx]\n", mappedpath, begin, end);
+	        offline_record_dso(mappedpath, begin, end, 0);
 	    }
 #endif
-	    offline_record_dso(mappedpath, begin, end, 0);
 	}
     }
     fclose(mapfile);
+#endif
 #endif
     return(0);
 }
---
+++ libopenss-runtime/offline/monlibs.h
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * ** Copyright (c) 2012 Matt LeGendre. All Rights Reserved.
+ * ** Copyright (c) 2012 The Krell Institue. All Rights Reserved.
+ * **
+ * ** This library is free software; you can redistribute it and/or modify it under
+ * ** the terms of the GNU Lesser General Public License as published by the Free
+ * ** Software Foundation; either version 2.1 of the License, or (at your option)
+ * ** any later version.
+ * **
+ * ** This library is distributed in the hope that it will be useful, but WITHOUT
+ * ** ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * ** FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * ** details.
+ * **
+ * ** You should have received a copy of the GNU Lesser General Public License
+ * ** along with this library; if not, write to the Free Software Foundation, Inc.,
+ * ** 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * *******************************************************************************/
+
+//#define _GNU_SOURCE
+//#define __USE_GNU
+#include <elf.h>
+#include <link.h>
+
+/* Never seen more than 3 load segments in practice. 16 should be fine. */
+#define MAX_LOAD_SEGMENTS 16
+typedef struct {
+   ElfW(Addr) mem_addr;
+   size_t mem_size;
+   ElfW(Off) file_offset;
+   size_t file_size;
+   unsigned int premissions; /* in rwx format */
+} mem_region;
+
+extern const char *monlibs_vdso_name; /* = "[vdso]" */
+extern const char *monlibs_exe_name;  /* = "[exe]" */
+
+/**
+ * Call monlibs_getLibraries and it will report all library changes since the last call 
+ * via its library_cb callback.
+ * 
+ * Each library loaded/unloaded will be reported with a call to library_cb
+ *  - base_address is the library's base address.  It can be added to symbol
+ *    offsets to get symbol addresses.
+ *  - name is the filename of the library.  The memory pointed to by name will
+ *    remain valid until immediately after the callback that reports the library
+ *    unload returns.
+ *    Note that instead of reporting names for the executable and linux VDSO,
+ *    monlibs respectively returns the monlibs_vsdo_name and monlibs_exe_name
+ *    pointers.
+ *  - regions is an array of mem_region structs.  Each mem_region describes 
+ *    a block of memory the library occupies, what parts of the file on disk
+ *    that memory was mapped from, and the premissions of the pages.  The memory
+ *    of regions array is invalidated after the callback returns.
+ *  - num_regions is the size of the regions array.
+ *  - is_load is '1' if this callback is reporting a library load, and '0' if it's
+ *    reporting a library unload.
+ **/
+typedef void(*library_cb)(ElfW(Addr) base_address, const char *name, mem_region *regions, unsigned int num_regions, int is_load);
+void monlibs_getLibraries(library_cb cb);
