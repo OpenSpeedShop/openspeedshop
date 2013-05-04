cd0dced5623c80067f4ab8da33566813cf12c5d6
jgalarowicz <>
2013-05-04 22:04:00 +0000

    Remove Eon from top level executables directory.

--- test/executables/Eon/bsd_time.h
+++
@@ -1,8 +0,0 @@
-
-/*
- * Structure used in select() call, taken from the BSD file sys/time.h.
- */
-struct timeval {
-        long    tv_sec;         /* seconds */
-        long    tv_usec;        /* and microseconds */
-};
--- test/executables/Eon/myrand.h
+++
@@ -1,5 +0,0 @@
-extern
-void my_srand(int i);
-
-extern
-long int my_rand();
