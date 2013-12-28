2594d7d9b9e206229d788b15d63f493bf5f21ce8
jgalarowicz <>
2013-12-28 00:01:11 +0000

    Forgot to update the copyright dates.

--- plugins/panels/PTHREADSPanel/PTHREADSPanel.cxx
+++ plugins/panels/PTHREADSPanel/PTHREADSPanel.cxx
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2005 Silicon Graphics, Inc. All Rights Reserved.
-// Copyright (c) 2006-2013 Krell Institute All Rights Reserved.
+// Copyright (c) 2006-2014 Krell Institute All Rights Reserved.
 //
 //
 // This library is free software; you can redistribute it and/or modify it under
