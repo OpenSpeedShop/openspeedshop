f04c7b22e97da785192eca6051c77a8e440fe380
jgalarowicz <>
2014-02-06 03:52:57 +0000

    Actually add the MPIP Panel to the repository.  Omitted in my original check-in.

---
+++ plugins/panels/MPIPPanel/MPIPPanel.hxx
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2005 Silicon Graphics, Inc. All Rights Reserved.
+// Copyright (c) 2006-2014 Krell Institute All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef MPIPPANEL_H
+#define MPIPPANEL_H
+#include "CustomExperimentPanel.hxx"           // Do not remove
+#include "Panel.hxx"           // Do not remove
+
+class PanelContainer;   // Do not remove
+
+class ArgumentObject;
+
+
+#undef PANEL_CLASS_NAME
+#define PANEL_CLASS_NAME MPIPPanel   // Change the value of the define
+                                         // to the name of your new class.
+
+class MPIPPanel  : public CustomExperimentPanel
+{
+  public: 
+    MPIPPanel(PanelContainer *pc, const char *n, ArgumentObject *ao);
+
+    ~MPIPPanel();
+};
+#endif // MPIPPANEL_H
