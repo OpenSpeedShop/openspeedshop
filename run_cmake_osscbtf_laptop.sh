889d3af6014713e4148c1e348c27c9fc562fa2fa
Gregory Schultz <gregory.schultz@embarqmail.com>
2016-06-11 01:13:25 -0500

    cmake file changes for my env.

---
+++ run_cmake_osscbtf_laptop.sh
@@ -0,0 +1,37 @@
+rm -rf build-cbtf
+mkdir build-cbtf
+pushd build-cbtf
+
+export KRELL_ROOT=/opt/DEVEL/krellroot_v2.2.2
+export MRNET_ROOT=/opt/DEVEL/krellroot_v2.2.2
+export DYNINST_ROOT=/opt/DEVEL/krellroot_v2.2.2
+export CBTF_KRELL_INSTALL_DIR=/opt/DEVEL2/cbtf_v2.2.2
+export CBTF_ARGO_INSTALL_DIR=/opt/DEVEL2/cbtf_v2.2.2
+export CBTF_INSTALL_DIR=/opt/DEVEL2/cbtf_v2.2.2
+export OSS_CBTF_INSTALL_DIR=/opt/DEVEL2/osscbtf_v2.2.2
+export XERCESC_ROOT=/opt/DEVEL/krellroot_v2.2.2
+
+# Locate the find package files in the locations that cmake expects
+export MY_CMAKE_PREFIX_PATH="${CBTF_INSTALL_DIR}:${CBTF_KRELL_INSTALL_DIR}:${KRELL_ROOT}"
+cmake .. \
+       -DCMAKE_BUILD_TYPE=None \
+       -DCMAKE_CXX_FLAGS="-g -O2" \
+       -DCMAKE_C_FLAGS="-g -O2" \
+       -DCMAKE_INSTALL_PREFIX=${OSS_CBTF_INSTALL_DIR}\
+       -DCBTF_DIR=${CBTF_INSTALL_DIR} \
+       -DCBTF_KRELL_DIR=${CBTF_KRELL_INSTALL_DIR} \
+       -DCBTF_ARGONAVIS_DIR=${CBTF_ARGO_INSTALL_DIR} \
+       -DINSTRUMENTOR="cbtf" \
+       -DRESOLVE_SYMBOLS=symtabapi \
+       -DCMAKE_PREFIX_PATH=${MY_CMAKE_PREFIX_PATH} \
+       -DBINUTILS_DIR=${KRELL_ROOT} \
+       -DLIBELF_DIR=${KRELL_ROOT} \
+       -DLIBDWARF_DIR=${KRELL_ROOT} \
+       -DDYNINST_DIR=${DYNINST_ROOT} \
+       -DXERCESC_DIR=${XERCESC_ROOT} \
+       -DMRNET_DIR=${MRNET_ROOT}
+
+make clean
+make
+make install
+
