3427f2c5028826ae6b02b9eba40b81e7777baf42
Jim Galarowicz <jeg@krellinst.org>
2016-11-18 08:06:07 -0800

    Clean up the iot calltree related views.  OSS had metrics that should not have been in that view.

--- plugins/views/iot/iot_view.cxx
+++ plugins/views/iot/iot_view.cxx
@@ -1,6 +1,6 @@
 /*******************************************************************************
 ** Copyright (c) 2006 Silicon Graphics, Inc. All Rights Reserved.
-** Copyright (c) 2006-2014 Krell Institute. All Rights Reserved.
+** Copyright (c) 2006-2016 Krell Institute. All Rights Reserved.
 **
 ** This library is free software; you can redistribute it and/or modify it under
 ** the terms of the GNU Lesser General Public License as published by the Free
@@ -897,7 +897,9 @@ static bool define_iot_columns (
     IV.push_back(new ViewInstruction (VIEWINST_Display_Percent_Tmp, last_column++, intime_temp, totalIndex++));
     HV.push_back("% of Total Inclusive Time");
   } else {
+
    // If nothing is requested ...
+
     if (vfc == VFC_Trace) {
       // Insert start and end times into report.
 #ifdef DEBUG_IOT
@@ -920,6 +922,7 @@ static bool define_iot_columns (
         Look_For_KeyWord(cmd, "CallTrees")) {
       generate_nested_accounting = true;
     }
+
     IV.push_back(new ViewInstruction (VIEWINST_Display_Tmp, last_column++, extime_temp));
 #ifdef DEBUG_IOT
     printf("iot_view, after for extime_temp=%d, last_column=%d\n", extime_temp, last_column);
@@ -938,6 +941,7 @@ static bool define_iot_columns (
     HV.push_back("% of Total I/O Time");
 
 #if 1
+   
     if (vfc == VFC_Trace) {
   // display function return values for each function
       IV.push_back(new ViewInstruction (VIEWINST_Display_Tmp, last_column++, retval_temp));
@@ -964,8 +968,10 @@ static bool define_iot_columns (
 
 // another attempt at getting the percentage of application time the I/O routine took
 #if 0
+      std::cerr << "pushing back elapsed_time=" << application_elapsed_time << std::endl;
       //IV.push_back(new ViewInstruction (VIEWINST_SetConstFloat, last_column++, application_elapsed_time,totalIndex++));
       IV.push_back(new ViewInstruction (VIEWINST_SetConstFloat, totalIndex, application_elapsed_time));
+      //IV.push_back(new ViewInstruction (VIEWINST_Display_Tmp, totalIndex, application_elapsed_time));
       IV.push_back(new ViewInstruction (VIEWINST_Display_Percent_Tmp, last_column++, extime_temp, totalIndex++));
       HV.push_back("% of Total App Time");
 #endif
@@ -983,7 +989,7 @@ static bool define_iot_columns (
 #endif
 
 #if 1
-    if (vfc != VFC_Trace) {
+    if (vfc != VFC_Trace && vfc != VFC_CallStack) {
       // display count of min bytes value
        IV.push_back(new ViewInstruction (VIEWINST_Display_Tmp, last_column++, minbytescount_temp));
        HV.push_back(std::string(" Min_Bytes Count") );
@@ -1002,11 +1008,8 @@ static bool define_iot_columns (
     }
 
 #endif
-   
 
   }
-
-
 
   if (generate_nested_accounting) {
     IV.push_back(new ViewInstruction (VIEWINST_StackExpand, intime_temp));
