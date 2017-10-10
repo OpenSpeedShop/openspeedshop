50699496637955910572d2ed4a6def5b5e15b199
Gregory Schultz <gregory.schultz@embarqmail.com>
2017-10-10 00:22:29 -0600

    Power8 development tweaks.

--- run_cmake_osscbtf_laptop.sh
+++ run_cmake_osscbtf_laptop.sh
@@ -4,14 +4,15 @@ rm -rf build-cbtf
 mkdir build-cbtf
 pushd build-cbtf
 
-export KRELL_ROOT=/opt/DEVEL/krellroot_v2.2.4
-export MRNET_ROOT=/opt/DEVEL/krellroot_v2.2.4
-export DYNINST_ROOT=/opt/DEVEL/krellroot_v2.2.4
-export CBTF_KRELL_INSTALL_DIR=/opt/DEVEL/cbtf_v2.2.4
-export CBTF_ARGO_INSTALL_DIR=/opt/DEVEL/cbtf_v2.2.4
-export CBTF_INSTALL_DIR=/opt/DEVEL/cbtf_v2.2.4
-export OSS_CBTF_INSTALL_DIR=/opt/DEVEL/osscbtf_v2.2.4
-export XERCESC_ROOT=/opt/DEVEL/krellroot_v2.2.4
+export KRELL_ROOT=~/OSS/krellroot_v2.3.1
+export MRNET_ROOT=~/OSS/krellroot_v2.3.1
+export DYNINST_ROOT=~/OSS/krellroot_v2.3.1
+export CBTF_KRELL_INSTALL_DIR=~/OSS/cbtf_v2.3.1
+export CBTF_ARGO_INSTALL_DIR=~/OSS/cbtf_v2.3.1
+export CBTF_INSTALL_DIR=~/OSS/cbtf_v2.3.1
+export OSS_CBTF_INSTALL_DIR=~/OSS/osscbtf_v2.3.1
+export XERCESC_ROOT=~/OSS/krellroot_v2.3.1
+export SQLITE3_ROOT=~/OSS/krellroot_v2.3.1
 
 # Locate the find package files in the locations that cmake expects
 export MY_CMAKE_PREFIX_PATH="${CBTF_INSTALL_DIR}:${CBTF_KRELL_INSTALL_DIR}:${KRELL_ROOT}"
@@ -27,10 +28,12 @@ cmake .. \
        -DRESOLVE_SYMBOLS=symtabapi \
        -DCMAKE_PREFIX_PATH=${MY_CMAKE_PREFIX_PATH} \
        -DBINUTILS_DIR=${KRELL_ROOT} \
+       -DBOOST_ROOT=${KRELL_ROOT} \
        -DLIBELF_DIR=${KRELL_ROOT} \
        -DLIBDWARF_DIR=${KRELL_ROOT} \
        -DDYNINST_DIR=${DYNINST_ROOT} \
        -DXERCESC_DIR=${XERCESC_ROOT} \
+       -DSQLITE3_DIR=${SQLITE3_ROOT} \
        -DMRNET_DIR=${MRNET_ROOT}
 
 make clean
