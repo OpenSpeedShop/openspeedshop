94cb3268b71c3f5c7476c3668bf4501dcd24edbf
Jim Galarowicz <jgalarowicz@newmexicoconsortium.org>
2019-01-08 11:46:59 -0600

    Fix bug in find libtirpc cmake file in openspeedshop.  Was partially finding libtirpc - the includes.

--- cmake/FindLibtirpc.cmake
+++ cmake/FindLibtirpc.cmake
@@ -6,26 +6,26 @@ find_path(Libtirpc_INCLUDE_DIR
   PATH_SUFFIXES tirpc
 )
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Libtirpc REQUIRED_VARS Libtirpc_INCLUDE_DIR)
 
 find_library(Libtirpc_LIBRARY_SHARED NAMES tirpc
     HINTS /usr
     PATH_SUFFIXES lib lib64
     )
 
-message(STATUS "LIBTIRPC_FOUND:" ${LIBTIRPC_FOUND})
-message(STATUS "Libtirpc_FOUND:" ${Libtirpc_FOUND})
-message(STATUS "Libtirpc_INCLUDE_DIR:" ${Libtirpc_INCLUDE_DIR})
+find_package_handle_standard_args(Libtirpc REQUIRED_VARS Libtirpc_LIBRARY_SHARED)
 
-if(LIBTIRPC_FOUND)
+message(STATUS "LIBTIRPC_FOUND:" ${LIBTIRPC_FOUND})
+
+if (LIBTIRPC_FOUND)
   set(Libtirpc_INCLUDE_DIRS ${Libtirpc_INCLUDE_DIR})
   set(Libtirpc_LIBRARIES ${Libtirpc_LIBRARY_SHARED})
   mark_as_advanced(
     Libtirpc_INCLUDE_DIRS Libtirpc_LIBRARIES
   )
-  message(STATUS "Libtirpc_INCLUDE_DIRS:" ${Libtirpc_INCLUDE_DIRS})
-  message(STATUS "Libtirpc_LIBRARIES:" ${Libtirpc_LIBRARIES})
+  #message(STATUS "Libtirpc_INCLUDE_DIRS:" ${Libtirpc_INCLUDE_DIRS})
+  #message(STATUS "Libtirpc_LIBRARIES:" ${Libtirpc_LIBRARIES})
 else()
+  set(LIBTIRPC_FOUND false)
   set(Libtirpc_INCLUDE_DIRS "")
   set(Libtirpc_LIBRARIES "")
 endif()
